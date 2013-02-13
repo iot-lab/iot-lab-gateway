96a153a144cb1bb983c5af2460caae9f61ca71a5
Alaeddine WESLATI <alaeddine.weslati@inria.fr>
2013-02-13 11:42:49 +0100

    added : UpdateFirmware class

--- FlashFirwmare.py
+++ FlashFirwmare.py
@@ -1,5 +1,32 @@
 #! /usr/bin/env python
 
+import subprocess
+import shlex
+
+class UpdateFirmware():
+  def __init__(self, cfg_file):
+    self.cfg_file = cfg_file
+
+  def flash(self, elf_file):
+    cmd = """
+      openocd -f "%s" 
+              -f "target/stm32f1x.cfg" 
+              -c "init" 
+              -c "targets" 
+              -c "reset halt" 
+              -c "reset init" 
+              -c "flash write_image erase %s" 
+              -c "verify_image %s" 
+              -c "reset run"
+              -c "shutdown"
+
+    """ % (self.cfg_file, self.elf_file, self.elf_file)
+
+    openocd = subprocess.Popen(shlex.split(cmd))
+    out, err = openocd.communicate()
+
+    print "Out : %s" % out
+    print "Err : %s" % err
 
 
 
