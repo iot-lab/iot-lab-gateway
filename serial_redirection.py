dca8bea618572b418a9d7776ce0a3c9e7d19b0b3
Gaëtan Harter <gaetan.harter@inria.fr>
2013-02-20 16:37:25 +0100

    Correcting the thread class name, only one _ is required.

--- serial_redirection.py
+++ serial_redirection.py
@@ -13,6 +13,7 @@ import shlex
 import threading
 import time
 import signal
+import inspect
 
 
 
@@ -20,14 +21,12 @@ import signal
 import config
 
 
-class __SerialRedirectionThread(threading.Thread):
+class _SerialRedirectionThread(threading.Thread):
 
-    def __init__(self, tty, baudrate = 500000, error_handler = None, handler_arg = None):
+    def __init__(self, tty, baudrate, error_handler, handler_arg):
 
 
-        #print super(__SerialRedirectionThread, self)
-        #super(__SerialRedirectionThread, self).__init__()
-        threading.Thread.__init__(self)
+        super(_SerialRedirectionThread, self).__init__()
 
         self.err = ""
         self.out = ""
@@ -48,13 +47,20 @@ class __SerialRedirectionThread(threading.Thread):
         self.current_redirector_process = None
 
     def start(self):
-        self.is_running = True
-        #super(__SerialRedirectionThread, self).start()
+        """
+        Start a new thread
+        """
 
-        threading.Thread.start(self)
+        self.is_running = True
+
+        super(_SerialRedirectionThread, self).start()
 
     def run(self):
-        print 'Run'
+        """
+        Run the thread
+
+        It starts a while loop running a socat command
+        """
 
         cmd = '''echo "test"'''
         cmd_list = shlex.split(cmd)
@@ -62,15 +68,28 @@ class __SerialRedirectionThread(threading.Thread):
         # Run openocd
 
         while not self.stop_thread:
-            self.current_redirector_process = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
-            time.sleep(0.2)
-            #self.current_redirector_process.wait()
-            out, err = self.current_redirector_process.communicate()
+            # Pre-run tests (and repeated errors handling maybe)
+            test_error = False
+            if test_error:
+                error_str = ""
+                if self.error_handler is not None:
+                    self.error_handler(handler_arg, error_str)
+                break;
 
+            self.current_redirector_process = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
+            out, err = self.current_redirector_process.communicate()
             self.out += out
             self.err += err
 
+            if self.current_redirector_process.returncode != 0:
+                time.sleep(0.2)
+
+            # debug
+            time.sleep(0.2)
+
         self.is_running = False
+
+
 
     def stop(self):
 
@@ -78,67 +97,87 @@ class __SerialRedirectionThread(threading.Thread):
 
         # kill
         while self.is_running:
-            self.current_redirector_process.terminate()
+            try:
+                self.current_redirector_process.terminate()
+            except OSError, e:
+                if e.errno == 3:
+                    # 'No such proccess'
+                    # It means that the current process is already terminated
+                    pass
+                else:
+                    raise e
             time.sleep(1)
 
         self.current_redirector_process = None
 
 
-# class SerialRedirection():
-#
-#
-#     def run(self, error_handler, handler_arg):
-#         """
-#         error_handler == method with 1 argument or none
-#         """
-#
-#         self.is_running = True
-#         self.error_handler = error_handler
-#         self.handler_arg = handler_arg
-#
-#         self.redirector_thread = threading.Thread(target=self.run)
-#         self.redirector_thread.start()
-#
-#
-#     def stop(self):
-#
-#
-#         self.is_running = False
-#
-#         self.error_handler = None
-#         self.handler_arg = None
-#
-#         # kill the current Popen
-#         # join thread
-#         self.redirector_thread = None
-#
-#
-#
-#         # logging ?
-#
-#
-#
-#     def run(self, ttyelf_file):
-#         cmd_list = shlex.split(cmd)
-#
-#         # Run openocd
-#         openocd = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
-#         out, err = openocd.communicate() # nothing is written to stdout
-#         ret = openocd.returncode
-#
-#         self.out = out
-#         self.err = err
-#
-#         # Check execution
-#         if ret != 0:
-#             # logging ???
-#             pass
-#
-#         return ret
-#
-#     def stop(self):
-#         pass
-#
+class SerialRedirection():
+
+    def __init__(self, tty, baudrate = 500000, error_handler = None, handler_arg = None):
+        """
+
+        error_handler signature:
+            def error_handler(handler_arg, error_string)
+        """
+
+        # check handler signature
+        if error_handler is not None:
+            if len(inspect.getargspec(error_handler)[0]) != 2:
+                raise ValueError, 'Error handler should accept two arguments'
+
+
+        self.redirector_thread = _SerialRedirectionThread(tty, baudrate, self.__error_handler, self)
+        self.error_handler = error_handler
+        self.handler_arg = handler_arg
+        self.is_running = False
+        self.err = None
+        self.out = None
+
+
+    def start(self):
+        """
+        Start a new SerialRedirection
+        """
+
+        if self.is_running:
+            # Already running
+            return 1
+
+        self.err = ""
+        self.out = ""
+
+        self.is_running = True
+        self.redirector_thread.start()
+
+        return 0
+
+
+    def stop(self):
+        """
+        Stop the current Serial Redirection
+        """
+
+        if not self.is_running:
+            # not running
+            return 1
+
+        self.redirector_thread.stop()
+        self.redirector_thread.join()
+        self.is_running = False
+
+        self.err = self.redirector_thread.err
+        self.out = self.redirector_thread.out
+
+
+        # logging ?
+        return 0
+
+    def __error_handler(self, error_str):
+        self.err = self.redirector_thread.err
+        self.out = self.redirector_thread.out
+
+        if self.error_handler is not None:
+            self.error_handler(self.handler_arg, error_str)
 
 
 def parse_arguments(args):
@@ -160,6 +199,11 @@ def parse_arguments(args):
 
 
 
+def error_handler_test(arg, error_str):
+    print "error_handler_test"
+    print arg
+    print error_str
+
 if __name__ == '__main__':
 
 
@@ -168,9 +212,15 @@ if __name__ == '__main__':
     if BAUDRATE is None:
         BAUDRATE = config.NODES_CFG[NODE]['baudrate']
 
+    TTY = config.NODES_CFG[NODE]['tty']
 
 
-    THREAD = __SerialRedirectionThread("LALA")
+
+
+
+#    def __init__(self, tty, baudrate = 500000, error_handler = None, handler_arg = None):
+
+    THREAD = SerialRedirection(TTY, BAUDRATE, error_handler_test)
 
     def signal_handler(signal, frame):
         print >> sys.stderr, 'Got Ctrl+C'
@@ -185,7 +235,6 @@ if __name__ == '__main__':
     print 'Stop'
     THREAD.stop()
     print 'stopped'
-    THREAD.join()
 
     print "Err:"
     print THREAD.err
