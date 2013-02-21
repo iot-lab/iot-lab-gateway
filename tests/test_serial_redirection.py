f3b04aa4affb911f67681b603c7e918e98fe426a
Gaëtan Harter <gaetan.harter@inria.fr>
2013-02-21 15:45:24 +0100

    add a second dummy test

--- tests/test_serial_redirection.py
+++ tests/test_serial_redirection.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
 
+
 def test_0_to_test_tester():
+    print "On est heureux, E-R-E"
+    return 0
+
+def test_1_to_test_tester():
     print "On est heureux, E-R-E"
     return 0
