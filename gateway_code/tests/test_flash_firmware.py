e691c3bbbe20bd063fb1888ab8629e792f7d743b
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-06 16:43:57 +0100

    Add some tests

--- gateway_code/tests/test_flash_firmware.py
+++ gateway_code/tests/test_flash_firmware.py
@@ -3,9 +3,44 @@
 import sys
 sys.path.append("../../")
 
+import re
+import mock
+
 from gateway_code import flash_firmware
 
-def test_0():
-    print "On est heureux, E-R-E"
+
+def test_init():
+    flash = flash_firmware.FlashFirmware('m3')
+    assert flash.node == 'm3'
+    assert re.search('fiteco-m3.cfg', flash.cfg_file)
+    assert flash.out == None
+    assert flash.err == None
+
     return 0
+
+
+
+class PopenMock():
+    def __init__(self, cmds, stdout, stderr):
+        print cmds
+        print stdout
+        print stderr
+
+    def communicate():
+        self.returncode = 0
+        assert False, 'Test assert'
+        return "STDOUT messages", "STDERR messages"
+
+
+
+@mock.patch('subprocess.Popen', PopenMock)
+def test_flash():
+    import subprocess
+    subprocess.Popn = PopenMock
+    flash = flash_firmware.FlashFirmware('m3')
+    ret = flash.flash('auauei')
+    return ret
+
+
+
 
