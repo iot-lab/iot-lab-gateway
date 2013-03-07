ff7d46f7ea56a44ef30d4786345ddc324a288f02
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-07 17:59:03 +0100

    Adding command line tests

--- gateway_code/tests/test_flash_firmware.py
+++ gateway_code/tests/test_flash_firmware.py
@@ -32,7 +32,7 @@ def test_flash_OK():
     filename = 'TEST_FILENAME'
 
     common.PopenMock.setup(out=out, err=err, returncode=returncode)
-    with mock.patch('subprocess.Popen', new=common.PopenMock):
+    with mock.patch('subprocess.Popen', common.PopenMock):
         ret = flash.flash('TEST_FILENAME')
 
     assert flash.out == out
@@ -50,7 +50,7 @@ def test_flash_Error():
     returncode = 42
 
     common.PopenMock.setup(out=out, err=err, returncode=returncode)
-    with mock.patch('subprocess.Popen', new=common.PopenMock):
+    with mock.patch('subprocess.Popen', common.PopenMock):
         ret = flash.flash('auauei')
 
     assert flash.out == out
@@ -59,5 +59,82 @@ def test_flash_Error():
 
 
 
+
+# Command line tests
+
+def test_error_no_arguments():
+    """
+    Running command line without arguments
+    """
+    from cStringIO import StringIO
+    try:
+        sys.stdout = captured_out = StringIO()
+        sys.stderr = captured_err = StringIO()
+        with mock.patch('sys.stdout', captured_out):
+            with mock.patch('sys.stderr', captured_err):
+                ret = flash_firmware.main(['flash_firmware.py'])
+    except SystemExit, ret:
+        assert ret.code != 0
+    else:
+        assert 0
+
+
+def test_error_help():
+    """
+    Running command line with --help
+    """
+    from cStringIO import StringIO
+    try:
+        captured_out = StringIO()
+        captured_err = StringIO()
+        with mock.patch('sys.stdout', captured_out):
+            with mock.patch('sys.stderr', captured_err):
+                ret = flash_firmware.main(['flash_firmware.py', '-h'])
+    except SystemExit, ret:
+        assert ret.code == 0
+    else:
+        assert 0
+
+
+@mock.patch.object(flash_firmware.FlashFirmware, 'flash', lambda s,x: 0)
+def test_normal_run():
+    """
+    Running command line with m3
+    """
+    from cStringIO import StringIO
+    try:
+        captured_out = StringIO()
+        captured_err = StringIO()
+        with mock.patch('sys.stdout', captured_out):
+            with mock.patch('sys.stderr', captured_err):
+                print 'Lala'
+                ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
+    except SystemExit, ret:
+        assert 0
+    else:
+        print captured_err.getvalue()
+        assert re.search('OK', captured_err.getvalue())
+        assert ret == 0
+
+
+# @mock.patch.object(flash_firmware.FlashFirmware, 'flash', lambda s,x: 1)
+# def test_error_run():
+#     """
+#     Running command line with m3
+#     """
+#     from cStringIO import StringIO
+#     try:
+#         captured_out = StringIO()
+#         captured_err = StringIO()
+#         with mock.patch('sys.stdout', captured_out):
+#             with mock.patch('sys.stderr', captured_err):
+#                 print 'Lala'
+#                 ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
+#     except SystemExit, ret:
+#         assert 0
+#     else:
+#         print captured_err.getvalue()
+#         assert re.search('OK', captured_err.getvalue())
+#         assert ret == 0
 
 
