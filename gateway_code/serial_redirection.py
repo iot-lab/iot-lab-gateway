a8ac2b04096d71b583ca2230409ee08981617337
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-11 18:16:52 +0100

    Adding test on init function of SerialRedirection

--- gateway_code/serial_redirection.py
+++ gateway_code/serial_redirection.py
@@ -20,105 +20,20 @@ import inspect
 from gateway_code import config
 
 
-class _SerialRedirectionThread(threading.Thread):
-
-    def __init__(self, tty, baudrate, error_handler, handler_arg):
-
-
-        super(_SerialRedirectionThread, self).__init__()
-
-        self.err = ""
-        self.out = ""
-
-        # serial link informations
-        self.tty = tty
-        self.baudrate = baudrate
-
-        # Handler called on error on socat
-        self.error_handler = error_handler
-        self.handler_arg = handler_arg
-
-        # Stopping thread
-        self.stop_thread = False
-        self.is_running = False
-
-        # Current process running socat
-        self.redirector_process = None
-
-    def start(self):
-        """
-        Start a new thread
-        """
-
-        self.is_running = True
-
-        super(_SerialRedirectionThread, self).start()
-
-    def run(self):
-        """
-        Run the thread
-
-        It starts a while loop running a socat command
-        """
-
-        cmd = '''echo "test"'''
-        cmd_list = shlex.split(cmd)
-
-        # Run openocd
-
-        while not self.stop_thread:
-            # Pre-run tests (and repeated errors handling maybe)
-            test_error = False
-            if test_error:
-                error_str = ""
-                if self.error_handler is not None:
-                    self.error_handler(self.handler_arg, error_str)
-                break
-
-            self.redirector_process = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
-            out, err = self.redirector_process.communicate()
-            self.out += out
-            self.err += err
-
-            if self.redirector_process.returncode != 0:
-                time.sleep(0.2)
-
-            # debug
-            time.sleep(0.2)
-
-        self.is_running = False
-
-
-
-    def stop(self):
-
-        self.stop_thread = True
-
-        # kill
-        while self.is_running:
-            try:
-                self.redirector_process.terminate()
-            except OSError, err:
-                if err.errno == 3:
-                    # 'No such proccess'
-                    # It means that the current process is already terminated
-                    pass
-                else:
-                    raise err
-            time.sleep(1)
-
-        self.redirector_process = None
-
-
 class SerialRedirection():
 
-    def __init__(self, tty, baudrate = 500000, \
+    def __init__(self, node,
             error_handler = None, handler_arg = None):
-        """
 
-        error_handler signature:
-            def error_handler(handler_arg, error_string)
         """
+        error_handler signature:
+            def error_handler(handler_arg, error_tuple)
+        error_tuple = (error_num, error_str)
+
+        """
+        if node not in config.NODES_CFG:
+            raise ValueError, 'Unknown node, not in %r' % config.NODES_CFG.keys()
+        self.node = node
 
         # check handler signature
         if error_handler is not None:
@@ -127,9 +42,14 @@ class SerialRedirection():
 
 
         self.redirector_thread = _SerialRedirectionThread(\
-                tty, baudrate, self.__error_handler, self)
+                config.NODES_CFG[self.node]['tty'],\
+                config.NODES_CFG[self.node]['baudrate'], \
+                self.__cb_error_handler,
+                self)
+
         self.error_handler = error_handler
         self.handler_arg = handler_arg
+
         self.is_running = False
         self.err = None
         self.out = None
@@ -173,12 +93,104 @@ class SerialRedirection():
         # logging ?
         return 0
 
-    def __error_handler(self, error_str):
+    def __cb_error_handler(self, error_str):
         self.err = self.redirector_thread.err
         self.out = self.redirector_thread.out
 
         if self.error_handler is not None:
             self.error_handler(self.handler_arg, error_str)
+
+class _SerialRedirectionThread(threading.Thread):
+
+    def __init__(self, node, error_handler):
+
+
+        super(_SerialRedirectionThread, self).__init__()
+
+        self.err = ""
+        self.out = ""
+
+        # serial link informations
+        self.node = node
+
+        # Handler called on error on socat
+        if error_handler is not None:
+            if len(inspect.getargspec(error_handler)[0]) != 1:
+                raise ValueError, 'Error handler should accept one argument'
+        self.error_handler = error_handler
+
+        # Stopping thread
+        self.stop_thread = False
+        self.is_running = False
+
+        # Current process running socat
+        self.redirector_process = None
+
+    def start(self):
+        """
+        Start a new thread
+        """
+
+        self.is_running = True
+
+        super(_SerialRedirectionThread, self).start()
+
+    def run(self):
+        """
+        Run the thread
+
+        It starts a while loop running a socat command
+        """
+
+        cmd = '''echo "Not Implemented"'''
+        cmd_list = shlex.split(cmd)
+
+        # Run openocd
+
+        while not self.stop_thread:
+            # Pre-run tests (and repeated errors handling maybe)
+            test_error = False
+            if test_error:
+                error_str = ""
+                if self.error_handler is not None:
+                    self.error_handler(error_str)
+                break
+
+            self.redirector_process = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
+            out, err = self.redirector_process.communicate()
+            self.out += out
+            self.err += err
+
+            if self.redirector_process.returncode != 0:
+                time.sleep(0.2)
+
+            # debug
+            time.sleep(0.2)
+
+        self.is_running = False
+
+
+
+    def stop(self):
+
+        self.stop_thread = True
+
+        # kill
+        while self.is_running:
+            try:
+                self.redirector_process.terminate()
+            except OSError, err:
+                if err.errno == 3:
+                    # 'No such proccess'
+                    # It means that the current process is already terminated
+                    pass
+                else:
+                    raise err
+            time.sleep(1)
+
+        self.redirector_process = None
+
+
 
 
 def parse_arguments(args):
@@ -193,55 +205,41 @@ def parse_arguments(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('node', type=str, choices=config.NODES,
             help="Node selection")
-    parser.add_argument('baudrate', type=int, default=500000, \
-            help="baudrate, default from config")
     arguments = parser.parse_args(args)
 
     return arguments.node, arguments.baudrate
 
 
 
-def error_handler_test(arg, error_str):
-    print "error_handler_test"
-    print arg
-    print error_str
+
+
+def main(args):
+    import mutex
+
+    def main_error_handler(arg, error_tuple):
+        print >> sys.stderr, "main_error_handler"
+        print >> sys.stderr, "arg: %r" % arg
+        print >> sys.stderr, "error: ('%d','%s')" % (error_tuple[0], error_tuple[1])
+        exit(error_tuple[0])
+
+    # main blocking mutex
+    main_mutex = mutex.mutex()
+    main_mutex.lock(None, None)
+    def cb_signal_handler(sig, frame):
+        print >> sys.stderr, 'Got Ctrl+C, Stopping...'
+        main_mutex.unlock()
+
+
+    node = parse_arguments(args)
+    thread = SerialRedirection(node, main_error_handler)
+
+    # Wait ctrl+C to stop
+    print 'Press Ctrl+C to stop the application'
+    signal.signal(signal.SIGINT, cb_signal_handler)
+    main_mutex.lock(thread.stop)
+    print >> sys.stderr, 'Stopped.'
+
 
 if __name__ == '__main__':
-
-
-    NODE, BAUDRATE = parse_arguments(sys.argv[1:])
-
-    if BAUDRATE is None:
-        BAUDRATE = config.NODES_CFG[NODE]['baudrate']
-
-    TTY = config.NODES_CFG[NODE]['tty']
-
-
-
-
-
-#    def __init__(self, tty, baudrate = 500000, 
-#           error_handler = None, handler_arg = None):
-
-    THREAD = SerialRedirection(TTY, BAUDRATE, error_handler_test)
-
-    def cb_signal_handler(sig, frame):
-        print >> sys.stderr, 'Got Ctrl+C'
-        THREAD.stop()
-
-    signal.signal(signal.SIGINT, cb_signal_handler)
-
-    print 'start'
-    THREAD.start()
-    print 'started'
-    time.sleep(10)
-    print 'Stop'
-    THREAD.stop()
-    print 'stopped'
-
-    print "Err:"
-    print THREAD.err
-    print "Out:"
-    print THREAD.out
-    print 'done'
+    main(sys.argv)
 
--- gateway_code/tests/test_serial_redirection.py
+++ gateway_code/tests/test_serial_redirection.py
@@ -1,12 +1,116 @@
 #! /usr/bin/env python
+# -*- coding:utf-8 -*-
 
 if __name__ == '__main__':
     import sys
     sys.path.append("../../")
 
-from gateway_code import serial_redirection
 
-def test_0():
-    print "On est heureux, E-R-E"
-    return 0
+from gateway_code import serial_redirection
+from gateway_code import config
+
+import mock
+import re
+
+_SerialRedirection_str = 'gateway_code.serial_redirection._SerialRedirectionThread'
+
+from gateway_code.serial_redirection import SerialRedirection
+class TestSerialRedirection:
+    """
+    Test the SerialRedirection class
+    """
+
+    def test_valid_init(self):
+        """
+        Test valid init calls
+        """
+        # Test init and handler_arg
+        for node in ('m3', 'gwt', 'a8'):
+            with mock.patch(_SerialRedirection_str) as mock_thread:
+                redirection = SerialRedirection(node)
+                assert redirection.node == node
+                assert redirection.is_running == False
+                # check calls to _SerialRedirectionThread
+                call_args = mock_thread.call_args_list
+                assert len(call_args) == 1
+                args = call_args[0][0]
+                assert config.NODES_CFG[node]['tty'] ==  args[0]
+                assert config.NODES_CFG[node]['baudrate'] == args[1]
+
+
+        # Test the error handler and handler_arg
+        with mock.patch(_SerialRedirection_str) as mock_thread:
+            def valid_error_handler(error_num, error_str):
+                pass
+            redirection = SerialRedirection(node, valid_error_handler, handler_arg = 'Test')
+            assert redirection.error_handler == valid_error_handler
+            assert redirection.handler_arg == 'Test'
+
+            redirection = SerialRedirection(node)
+            assert redirection.error_handler is None
+            assert redirection.handler_arg is None
+
+
+
+    def test_invalid_init(self):
+        """
+        Test Invalid init calls
+        """
+        with mock.patch(_SerialRedirection_str) as mock_thread:
+            mock_thread.side_effect = Exception('Called with invalid args')
+            try:
+                node = 'FALS_NODE_NAME'
+                redirection = SerialRedirection(node)
+            except ValueError as error:
+                assert re.search('Unknown node', error.message)
+            else:
+                assert 0
+
+            try:
+                node = 'm3'
+                def false_error_handler(only_one_arg):
+                    pass
+                redirection = SerialRedirection(node, false_error_handler)
+            except ValueError as error:
+                assert re.search('Error handler', error.message)
+            else:
+                assert 0
+
+
+    def test_start(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+#@mock.patch('subprocess.Popen', common.PopenMock)
+# class Test_SerialRedirectionThread:
+#     """
+#     Tests the private _SerialRedirectionThread class
+#     """
+# 
+#     def test_init(self):
+#         """
+#         Test Object creation
+#         """
+# 
+#         node = 'm3'
+#         def __error_handler_one_arg(error_str):
+#             pass
+#         error_handler = __error_handler_one_arg
+# 
+#         redirect_thread = serial_redirection._SerialRedirectionThread(\
+#                 tty, baudrate, error_handler)
+#         assert redirect_thread.tty == tty
+#         assert redirect_thread.baudrate == baudrate
+#         assert redirect_thread.error_handler == error_handler
+
+
 
