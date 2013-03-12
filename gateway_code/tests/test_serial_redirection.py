85c44d7a4fbac4ffce1877c44f99f39f14c28dc6
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-12 18:37:47 +0100

    Use reset_mock to cleanup the code, and check that mock are actually called

--- gateway_code/tests/test_serial_redirection.py
+++ gateway_code/tests/test_serial_redirection.py
@@ -16,68 +16,83 @@ from gateway_code import serial_redirection, config
 
 _SerialRedirection_str = 'gateway_code.serial_redirection._SerialRedirectionThread'
 
+@mock.patch(_SerialRedirection_str)
 class TestSerialRedirection:
     """
     Test the SerialRedirection class
     """
 
-    def test_valid_init(self):
+    def test_valid_init(self, mock_thread):
         """
         Test valid init calls
         """
         # Test init and handler_arg
         for node in ('m3', 'gwt', 'a8'):
-            with mock.patch(_SerialRedirection_str) as mock_thread:
-                redirection = SerialRedirection(node)
-                assert redirection.node == node
-                assert redirection.is_running == False
-                # check calls to _SerialRedirectionThread
-                call_args = mock_thread.call_args_list
-                assert len(call_args) == 1
-                args = call_args[0][0]
-                assert config.NODES_CFG[node]['tty'] ==  args[0]
-                assert config.NODES_CFG[node]['baudrate'] == args[1]
-
-
-        # Test the error handler and handler_arg
-        with mock.patch(_SerialRedirection_str) as mock_thread:
-            def valid_error_handler(error_num, error_str):
-                pass
-            redirection = SerialRedirection(node, valid_error_handler, handler_arg = 'Test')
-            assert redirection.error_handler == valid_error_handler
-            assert redirection.handler_arg == 'Test'
+            mock_thread.reset_mock()
 
             redirection = SerialRedirection(node)
-            assert redirection.error_handler is None
-            assert redirection.handler_arg is None
+            assert redirection.node == node
+            assert redirection.is_running == False
+
+            print mock_thread.call_count
+            assert mock_thread.call_count == 1
+
+            call_args = mock_thread.call_args_list[0][0]
+            assert config.NODES_CFG[node]['tty'] ==  call_args[0]
+            assert config.NODES_CFG[node]['baudrate'] == call_args[1]
+
+
+    def test_error_handler_and_arg(self, mock_thread):
+        """
+        Test calling with an error handler and an argument
+        """
+        valid_error_handler = (lambda x,y:0)
+
+        redirection = SerialRedirection('m3', valid_error_handler, handler_arg = 'Test')
+        assert redirection.error_handler == valid_error_handler
+        assert redirection.handler_arg == 'Test'
+
+        assert mock_thread.call_count == 1
 
 
 
-    def test_invalid_init(self):
+    def test_no_error_handler(self, mock_thread):
+        """
+        Test using without error handler/arg
+        """
+        redirection = SerialRedirection('m3')
+        assert redirection.error_handler is None
+        assert redirection.handler_arg is None
+
+        assert mock_thread.call_count == 1
+
+
+
+    def test_invalid_init(self, mock_thread):
         """
         Test Invalid init calls
         """
-        with mock.patch(_SerialRedirection_str) as mock_thread:
-            mock_thread.side_effect = Exception('Called with invalid args')
-            try:
-                node = 'FALS_NODE_NAME'
-                redirection = SerialRedirection(node)
-            except ValueError as error:
-                assert re.search('Unknown node', str(error))
-            else:
-                assert 0
+        mock_thread.side_effect = Exception('Should not have been called')
 
-            try:
-                node = 'm3'
-                def false_error_handler(only_one_arg):
-                    pass
-                redirection = SerialRedirection(node, false_error_handler)
-            except ValueError as error:
-                assert re.search('Error handler', str(error))
-            else:
-                assert 0
+        # invalid node name
+        try:
+            node = 'FALS_NODE_NAME'
+            redirection = SerialRedirection(node)
+        except ValueError as error:
+            assert re.search('Unknown node', str(error))
+        else:
+            assert 0
+
+        # invalid error handlers
+        try:
+            false_error_handler = (lambda x:0)
+            redirection = SerialRedirection('m3', false_error_handler)
+        except ValueError as error:
+            assert re.search('Error handler', str(error))
+        else:
+            assert 0
 
 
-    def test_start(self):
+    def test_start(self, mock_thread):
         pass
 
