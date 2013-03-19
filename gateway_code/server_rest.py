151d011781530e861042805c319ebcb630259ec7
Saint-Marcel frederic <frederic.saint-marcel@inria.fr>
2013-03-19 14:51:28 +0100

    add server rest bottle

---
+++ gateway_code/server_rest.py
@@ -0,0 +1,42 @@
+from bottle import route,run,template,post,request
+
+@route('/hello/:name')
+def index(name='World'):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@post('/exp/start/:expid/:username')
+def exp_start():
+    files = request.files.keys()
+    values = request.files.values()
+    for field in values:
+        filename = field.filename
+        data = field.file.read()
+        file = open(filename, 'w')
+        file.write(data)
+        file.close()
+    return str('experiment %s started' % exp_id )
+
+def parse_arguments(args):
+    """
+    Parsing arguments:
+
+    host port
+    Only pass arguments to function without script name
+
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', type=str,
+            help="Server address to bind to")
+    parser.add_argument('port', type=int, help="Server port to bind to")
+    arguments = parser.parse_args(args)
+
+    return arguments.host, arguments.port
+
+def main(args):
+    """
+    Command line main function
+    """
+    host, port = parse_arguments(args[1:])
+    run(host=host, port=port, server='paste')
