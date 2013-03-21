218fe5c0aa1de03c6e05a3ee7d30cb383bcf06a8
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-21 11:31:23 +0100

    Change GatewayManager to a global variable, change argument check in exp_start

--- gateway_code/server_rest.py
+++ gateway_code/server_rest.py
@@ -12,26 +12,7 @@ from tempfile import NamedTemporaryFile
 
 import json
 
-@post('/open/flash')
-def open_flash():
-    """
-    Flash open node
-
-    """
-    files_d = request.files
-    # verify passed files as request
-    if (len(files_d) > 1 or not 'firmware' in files_d):
-        return "Wrong file arguments, should be 'firmware' and only one file"
-
-    firmware = files_d['firmware']
-    manager = GatewayManager()
-
-    with NamedTemporaryFile(suffix = '--' + firmware.filename) as _file:
-        _file.write(firmware.file.read())
-        print "Start Open Node flash"
-        ret_tuple = manager.open_flash(_file.name)
-
-    return str(ret_tuple)
+MANAGER = GatewayManager()
 
 @post('/exp/start/:expid/:username')
 def exp_start(expid, username):
@@ -41,21 +22,38 @@ def exp_start(expid, username):
     :param expid: experiment id
     :param username: username of the experiment owner
     """
-
-    files_d = request.files
-
     # verify passed files as request
-    if set(files_d.keys()) != set(('firmware', 'profile')):
+    if set(request.files.keys()) != set(('firmware', 'profile')):
         return "Wrong file arguments, should be 'firmware' and 'profile'"
-    firmware = files_d['firmware']
-    profile = files_d['profile']
-    profile_obj = json.load(profile.file)
 
-    manager = GatewayManager()
+    firmware = request.files['firmware']
+    profile  = request.files['profile']
+    profile_obj = json.load(profile.file)
 
     with NamedTemporaryFile(suffix = '--' + firmware.filename) as _file:
         _file.write(firmware.file.read())
-        ret_tuple = manager.exp_start(expid, username, _file.name, profile_obj)
+        ret_tuple = MANAGER.exp_start(expid, username, _file.name, profile_obj)
+    return str(ret_tuple)
+
+@post('/open/flash')
+def open_flash():
+    """
+    Flash open node
+
+    Requires:
+    request.files contains 'firmware' file argument
+
+    """
+    # verify passed files as request
+    if set(filed_d.keys()) != set(('firmware')):
+        return "Wrong file arguments, should be 'firmware'"
+    firmware = request.files['firmware']
+
+    print "Start Open Node flash"
+    with NamedTemporaryFile(suffix = '--' + firmware.filename) as _file:
+        _file.write(firmware.file.read())
+        ret_tuple = MANAGER.open_flash(_file.name)
+
     return str(ret_tuple)
 
 
