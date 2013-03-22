b3daeb995d2bc170a822c04ed2325999acf493bb
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-22 16:14:30 +0100

    Add reset class

---
+++ gateway_code/tests/test_reset.py
@@ -0,0 +1,154 @@
+#! /usr/bin/env python
+
+
+import sys
+
+import re
+import mock
+from cStringIO import StringIO
+
+# import package code from source folder if not installed
+from os.path import dirname, abspath
+current_folder = dirname(abspath(__file__))
+source_folder = dirname(dirname(current_folder))
+sys.path.append(source_folder)
+
+from gateway_code import reset
+
+
+from subprocess import PIPE
+@mock.patch('subprocess.Popen')
+class TestsResetMethods:
+    """
+    Tests reset functions
+    """
+    def test_node_detection(self, mock_popen):
+        """
+        Test node detection
+        """
+        # config mock
+        popen = mock_popen.return_value
+        popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
+        popen.returncode = mock_ret = 0
+
+        # valid nodes
+        for node in ('m3', 'gwt', 'a8'):
+            ret, out, err = reset.reset(node)
+
+        # invalid nodes
+        try:
+            node = 'INEXISTANT_ NODE'
+            ret, out, err = reset.reset(node)
+        except:
+            pass
+        else:
+            assert 0, 'Non existant node not detected'
+
+
+    def test_reset_OK(self, mock_popen):
+        """
+        successfull reset
+        """
+        # config mock
+        popen = mock_popen.return_value
+        popen.returncode = mock_ret = 0
+        popen.communicate.return_value = \
+                (mock_out, mock_err) = ("OUT_MSG", "")
+
+        ret, out, err = reset.reset('m3')
+
+        assert popen.communicate.call_count == 1
+        assert mock_out == out
+        assert mock_err == err
+        assert mock_ret == ret
+
+
+    def test_reset_Error(self, mock_popen):
+        """
+        Test with a reset with a unsuccessfull call
+        """
+
+        popen = mock_popen.return_value
+        popen.returncode = mock_ret = 42
+        popen.communicate.return_value = \
+                (mock_out, mock_err) = ("OUT_ERR", "ERR_ERR")
+
+        filename = 'TEST_FILENAME'
+        ret, out, err = reset.reset('m3')
+
+        assert popen.communicate.call_count == 1
+        assert mock_out == out
+        assert mock_err == err
+        assert mock_ret == ret
+
+
+
+
+
+# Command line tests
+
+from cStringIO import StringIO
+captured_out = StringIO()
+captured_err = StringIO()
+@mock.patch('sys.stdout', captured_out)
+@mock.patch('sys.stderr', captured_err)
+class TestsCommandLineCalls:
+    def test_error_no_arguments(self):
+        """
+        Running command line without arguments
+        """
+        try:
+            ret = reset.main(['reset.py'])
+        except SystemExit as ret:
+            assert ret.code != 0
+        else:
+            assert 0
+
+
+    def test_error_help(self):
+        """
+        Running command line with --help
+        """
+        try:
+            ret = reset.main(['reset.py', '-h'])
+        except SystemExit as ret:
+            assert ret.code == 0
+        else:
+            assert 0
+
+
+
+
+    @mock.patch.object(reset, 'reset')
+    def test_normal_run(self, mock_fct):
+        """
+        Running command line with m3
+        """
+        mock_fct.return_value = (0, 'OUTMessage', 'ErrorRunMessage')
+        try:
+            ret = reset.main(['reset.py', 'm3'])
+        except SystemExit, ret:
+            assert 0
+        else:
+            assert re.search('OK', captured_err.getvalue())
+            assert ret == 0
+        assert mock_fct.called
+
+
+    @mock.patch.object(reset, 'reset')
+    def test_error_run(self, mock_fct):
+        """
+        Running command line with error during run
+        """
+        mock_fct.return_value = (42, 'OUT', 'ErrorRunMessage')
+        try:
+            ret = reset.main(['reset.py', 'm3'])
+        except SystemExit, ret:
+            assert 0
+        else:
+            err = captured_err.getvalue()
+            assert ret == 42
+        assert mock_fct.called
+
+
+
