eb20ee7739dd70014e9127ebc80f773635d9bdb1
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-26 17:49:55 +0100

    Test simple cases

--- gateway_code/tests/test_io.py
+++ gateway_code/tests/test_io.py
@@ -6,37 +6,86 @@ import mock
 
 from gateway_code import envoi
 import Queue
+import serial
+import sys
 
 
-@mock.patch('serial.serial')
-def test_rx(serial):
+@mock.patch('serial.Serial')
+def test_rx_multiple_packets(serial_mock_class):
 
     rx_queue = Queue.Queue(0)
 
     # configure test
-    read_values = [ envoie.SYNC_BYTE, chr(3), 'ab', 'c', '']
-    result_packets = ['abc']
+    read_values = [envoi.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
+            [envoi.SYNC_BYTE, chr(1), 'x'] + \
+            [envoi.SYNC_BYTE, chr(2), envoi.SYNC_BYTE, envoi.SYNC_BYTE]
+    result_packets = ['abc', 'x', envoi.SYNC_BYTE + envoi.SYNC_BYTE]
 
-    serial_mock = serial.return_value
-    serial_mock.read.side_effect = lambda: read_values.pop(0)
+    def read_mock():
+        if read_values == []:
+            raise ValueError
+        return read_values.pop(0)
+
+    serial_mock = serial_mock_class.return_value
+    serial_mock.read.side_effect = read_mock
 
     # Run thread
-    rx_thread = envoie.ThreadRead(rx_queue)
+    rx_thread = envoi.ThreadRead(rx_queue)
     rx_thread.start()
 
     rec_l = []
     try:
-        while True:
-            rx = rx_queue.get(block=True, timeout=2)
-            rec_l.append(rx)
-    except Queue.empty as error:
-        pass
+        for res_packet in result_packets:
+            rx = rx_queue.get(block=True, timeout=1)
+            assert rx == res_packet
+    except Queue.Empty as error: # will happen on timeout
+        assert 0
 
     # cleanup
     rx_thread.join()
 
-    print rec_l, result_packets
-    assert rec_l == result_packets
+@mock.patch('serial.Serial')
+def test_rx_wih_invalid_start(serial_mock_class):
+
+    rx_queue = Queue.Queue(0)
+
+    # configure test
+    read_values = ['a', 'b', 'c'] + \
+            [envoi.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
+            [envoi.SYNC_BYTE, chr(1), 'x'] + \
+            [envoi.SYNC_BYTE, chr(2), envoi.SYNC_BYTE, envoi.SYNC_BYTE]
+    result_packets = ['abc', 'x', envoi.SYNC_BYTE + envoi.SYNC_BYTE]
+
+    def read_mock():
+        if read_values == []:
+            raise ValueError
+        return read_values.pop(0)
+
+    serial_mock = serial_mock_class.return_value
+    serial_mock.read.side_effect = read_mock
+
+    # Run thread
+    rx_thread = envoi.ThreadRead(rx_queue)
+    rx_thread.start()
+
+    rec_l = []
+    try:
+        for res_packet in result_packets:
+            rx = rx_queue.get(block=True, timeout=1)
+            assert rx == res_packet
+    except Queue.Empty as error: # will happen on timeout
+        assert 0
+
+    # cleanup
+    rx_thread.join()
+
+
+
+
+
+
+
+
 
 
 
