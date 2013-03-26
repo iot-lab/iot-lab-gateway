ce8f768e042b8625c6e2864c4f7d8cf4573e8276
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-26 12:43:32 +0100

    Move Sandrine's code to module

--- gateway_code/envoi.py
+++ gateway_code/envoi.py
@@ -6,11 +6,12 @@ import pdb
 import threading
 from threading import Thread, Lock
 
-from gateway_loggin import logger
+
+from gateway_code.gateway_logging import logger
 
 SYNC_BYTE = chr(0x80)
 
-#The port is immediately opened on object creation bevause the port is given. 
+#The port is immediately opened on object creation bevause the port is given.
 serial_port = serial.Serial('/dev/ttyFITECO_GWT', 500000, timeout=16)
 
 #Queue can store 1 item
@@ -18,11 +19,11 @@ rx_queue = Queue(1)
 protect_send = Lock()
 
 
-#Definitions for the state machine to monitor the completion progress of 
+#Definitions for the state machine to monitor the completion progress of
 #a packet.
 RX_IDLE  = 1
 RX_LEN = 2
-RX_TYPE = 3 
+RX_TYPE = 3
 RX_PAYLOAD = 4
 
 #Definitions used to trigger the creation of a new Buffer to receive the packet.
@@ -38,19 +39,19 @@ class Buffer():
         self.length = None
         self.pkt_type = None
         self.payload = None
-        
+
     def __repr__(self):
         result = "sync id=%s, length=%s, type=%s, payload=%s" % \
                 (self.sync, self.length, self.pkt_type, self.payload)
         return result
-    
+
     def __len__(self):
         return len(self.sync) + len(self.length) + len(self.pkt_type) + \
                 len(self.payload)
 
 
 
-        
+
 def rx_idle (packet, rx_char):
     """Adds the sync byte to the packet and changes the rx_State
     to RX_LEN in order to get the next length byte."""
@@ -58,7 +59,7 @@ def rx_idle (packet, rx_char):
         packet.sync = SYNC_BYTE
         rx_state = RX_LEN
     return
-        
+
 def rx_length(packet, rx_char):
     """Puts the legth byte into the packet, changes the rx_state
     to RX_TYPE to get the packet type. """
@@ -67,7 +68,7 @@ def rx_length(packet, rx_char):
     return
 
 def rx_type(packet, rx_char):
-    """ Puts the packet type into the packet, changes the rx_state to 
+    """ Puts the packet type into the packet, changes the rx_state to
     RX_PAYLOAD to get the packet data."""
     packet.pkt_type = rx_char
     rx_state = RX_PAYLOAD
@@ -81,55 +82,55 @@ def rx_payload(packet, rx_char):
         packet.payload = rx_char
     else:
         packet.payload += rx_char
-        
+
     if len(packet) == packet.length:
         rx_state = RX_IDLE
         logger.debug("\t rx_payload packet : %s" %(packet))
-        
+
     return
-    
-   
-state_machine_dict = {  RX_IDLE: rx_idle, 
+
+
+state_machine_dict = {  RX_IDLE: rx_idle,
                             RX_LEN : rx_length,
                             RX_TYPE: rx_type,
                             RX_PAYLOAD: rx_payload,
-                            }   
-   
+                            }
+
 def receive_packet():
     #Lock on the serial link whe
     #SYNC  |  LENGTH | TYPE  | PAYLOAD |
-  
-    buffer_use = UNUSED   
-                 
+
+    buffer_use = UNUSED
+
     while True:
         #call to read will block when no bytes are received
-        rx_bytes = serial_port.read() 
-        
+        rx_bytes = serial_port.read()
+
         #New packet is being received, we get a new buffer
         if buffer_use == UNUSED:
             packet = Buffer()
             buffer_use = IN_USE
-            
+
         #TODO passer tout ce qui est recu aux fonctions au lieu
-        #de passer les char un par un 
+        #de passer les char un par un
         for rx_char in rx_bytes:
-            #Putting the bytes recived into the packet depending on the 
+            #Putting the bytes recived into the packet depending on the
             #reception state (rx_state)
             state_machine_dict[rx_state](packet, rx_char)
-            
+
         if rx_state == RX_IDLE:
             buffer_use = UNUSED
             #packet complete
             try:
-                rx_queue.put(packet) 
-                
+                rx_queue.put(packet)
+
             #TODO : ermplir condition queue full, bloquer sur le put?
             except Queue.Full:
                 pass
-               
-  
-        
-      
+
+
+
+
 
 
 RXThread = threading.Thread(group=None, target=receive_packet, \
@@ -137,26 +138,26 @@ RXThread = threading.Thread(group=None, target=receive_packet, \
 RXThread.start()
 
 def make_header(payload):
-    #payload is a string, and we add 2 bytes with 
+    #payload is a string, and we add 2 bytes with
     #elnght and sync byte
     #TYPE  included in payload?
     length = len(payload) + 3
-    
+
     packet = SYNC_BYTE + str(length) + payload
     return packet
-    
-    
-    
+
+
+
 
 def send_packet(payload):
-    
+
     protect_send.acquire()
-    
+
     tx_packet = make_header(payload)
     serial_port.write(tx_packet)
-    
+
     rx_packet = rx_queue.get(block=True)
     protect_send.release()
-    
+
     return rx_packet
-    
+
