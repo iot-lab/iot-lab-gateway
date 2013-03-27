ce2af36a8adcd9227ec7f693b1af174f6cde5174
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-27 18:44:01 +0100

    Refactor gateway_logging without class and without pylint warnings

--- gateway_code/gateway_logging.py
+++ gateway_code/gateway_logging.py
@@ -1,42 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
 
+"""
+
+Logger for gateway code
+
+Set 'logfile' path with 'add_rotating_handler'.
+
+Get logger with 'logger()'
+
+
+"""
+
+import logging
 from logging.handlers import RotatingFileHandler
 
-class GtwLogger():
-    def __init__(self, name ):
+LOGGER = logging.getLogger()
 
-        # création de l'objet logger qui va nous servir à écrire dans les logs
-        self.logger = logging.getLogger(name)
+# on met le niveau du logger à DEBUG, comme ça il écrit tout
+LOGGER.setLevel(logging.DEBUG)
 
-        # on met le niveau du logger à DEBUG, comme ça il écrit tout
-        self.logger.setLevel(logging.DEBUG)
 
-        logfile = "/var/log/gateway.log"
-        # création d'un formateur qui va ajouter le temps, le niveau
-        # de chaque message quand on écrira un message dans le log
-        self.formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-        # création d'un handler qui va rediriger une écriture du log vers
-        # un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
+FORMATTER = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+# création d'un handler qui va rediriger une écriture du log vers
+# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
 
-        self.file_handler = RotatingFileHandler(logfile, 'a', maxBytes=1000000, backupCount=1)
-        # on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
-        # créé précédement et on ajoute ce handler au logger
-        self.file_handler.setLevel(logging.DEBUG)
-        self.file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
+def add_rotating_handler(logfile):
+    """
+    Add a rotating file handler
+    """
+    file_handler = RotatingFileHandler(logfile, 'a', \
+           maxBytes=1000000, backupCount=1)
 
-        # création d'un second handler qui va rediriger chaque écriture de log
-        # sur la console
-        #stream_handler = logging.StreamHandler()
-        #stream_handler.setLevel(logging.DEBUG)
-        #logger.addHandler(stream_handler)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(FORMATTER)
 
-        # Après 3 heures, on peut enfin logguer
-        # Il est temps de spammer votre code avec des logs partout :
-        #logger.info('Hello')
-        #logger.warning('Testing %s', 'foo')
+    LOGGER.addHandler(file_handler)
 
-logger = GtwLogger('gtw_log')
+def logger():
+    """
+    :return: the logger
+    """
+
+    return LOGGER
+
+
+# création d'un second handler qui va rediriger chaque écriture de log
+# sur la console
+#stream_handler = logging.StreamHandler()
+#stream_handler.setLevel(logging.DEBUG)
+#LOGGER.addHandler(stream_handler)
+
+# Après 3 heures, on peut enfin logguer
+# Il est temps de spammer votre code avec des logs partout :
+#LOGGER.info('Hello')
+#LOGGER.warning('Testing %s', 'foo')
+
