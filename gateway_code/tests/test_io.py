ff41cea764cb0b0a869502d6dc1420ab17a31484
Sandrine Avakian <sandrine.avakian@inria.fr>
2013-03-27 15:09:21 +0100

    Renaming thread class into ReceiveThread. Creating RxTxSerial class. Refactoring code.

--- gateway_code/tests/test_io.py
+++ gateway_code/tests/test_io.py
@@ -30,7 +30,7 @@ def test_rx_multiple_packets(serial_mock_class):
     serial_mock.read.side_effect = read_mock
 
     # Run thread
-    rx_thread = envoi.ThreadRead(rx_queue)
+    rx_thread = envoi.ReceiveThread(rx_queue)
     rx_thread.start()
 
     rec_l = []
@@ -50,7 +50,7 @@ def test_rx_wih_invalid_start(serial_mock_class):
     rx_queue = Queue.Queue(0)
 
     # configure test
-    read_values = ['a', 'b', 'c'] + \
+    read_values = ['D', 'D', 'E','K','K'] + \
             [envoi.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
             [envoi.SYNC_BYTE, chr(1), 'x'] + \
             [envoi.SYNC_BYTE, chr(2), envoi.SYNC_BYTE, envoi.SYNC_BYTE]
@@ -65,7 +65,7 @@ def test_rx_wih_invalid_start(serial_mock_class):
     serial_mock.read.side_effect = read_mock
 
     # Run thread
-    rx_thread = envoi.ThreadRead(rx_queue)
+    rx_thread = envoi.ReceiveThread(rx_queue)
     rx_thread.start()
 
     rec_l = []
