ffb57c7e8f1ba765d8886c197eb9a493ffea28cc
Gaëtan Harter <gaetan.harter@inria.fr>
2013-03-28 18:23:39 +0100

    Correct the read_values array, and tests multiple values

--- gateway_code/dispatch.py
+++ gateway_code/dispatch.py
@@ -5,23 +5,23 @@ class Dispatch():
     OML_PKT_TYPE = chr(0xFF)
     def __init__(self, queue_oml, io_write = None):
         self.queue_oml = queue_oml
-        self.queue_control_node = Queue.Queue(1) 
+        self.queue_control_node = Queue.Queue(1)
         self.io_write = io_write
         self.protect_send = Lock()
-        
-    
+
+
     def cb_dispatcher(self, packet):
-        """ Forwards the packet tot the appropriate queue depending on 
+        """ Forwards the packet tot the appropriate queue depending on
         the packet type """
         #check packet type (first byte)
         if packet[0] == self.OML_PKT_TYPE:
             self.queue_oml.put(packet)
         else:
-            #put the control node's answer into the queue, unlocking 
+            #put the control node's answer into the queue, unlocking
             #send_command
             self.queue_control_node.put(packet)
-            
-            
+
+
     def send_command(self, data):
         """
         Send a packet and wait for the answer
@@ -33,7 +33,7 @@ class Dispatch():
         answer_cn = self.queue_control_node.get(block=True)
         self.protect_send.release()
         return answer_cn
-        
-        
-        
-        
+
+
+
+
