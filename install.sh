e816fd29261dc41314d4a632876f00dc5689d340
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-10 14:56:17 +0200

    Add init.d script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -ex
+
+
+
+python setup.py nosetests
+python setup.py lint
+python setup.py install
+
+
+# install daemon script
+cp  gateway-server-daemon  /etc/init.d
+chmod  +x  /etc/init.d/gateway-server-daemon
+
+# allow gateway-server to use serial ports
+usermod  -G  dialout  www-data
