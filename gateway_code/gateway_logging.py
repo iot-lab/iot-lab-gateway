0d094dd599734a21f71ac1d7af2d2c4d704736e8
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-12 11:23:38 +0200

    Add folder configuration to gateway_logger

--- gateway_code/gateway_logging.py
+++ gateway_code/gateway_logging.py
@@ -14,43 +14,34 @@ from logging.handlers import RotatingFileHandler
 
 # set default logger level to DEBUG to log everything
 LOGLEVEL = logging.DEBUG
-
-LOG_FOLDER = "/var/log/gateway-server/"
-
-
-
-LOGGER = logging.getLogger("gateway_logger")
-LOGGER.setLevel(LOGLEVEL)
-
+LOGGER_NAME = 'gateway_logger'
 FORMATTER = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
+def init_logger(log_folder):
+    """
+    Create global logger and handlers
 
+    :param log_folder: destination folder for the logs
+    """
 
+    log_folder += '/'
 
+    logger = logging.getLogger(LOGGER_NAME)
+    #logger.setLevel(LOGLEVEL)
 
-# Server logs
-LOGFILE = LOG_FOLDER + "gateway-server.log"
-FILE = RotatingFileHandler(LOGFILE, 'a', maxBytes=1000000, backupCount=1)
+    # Server logs
+    server_f = log_folder + 'gateway-server.log'
+    server = RotatingFileHandler(server_f, 'a', maxBytes=1000000, backupCount=1)
+    server.setLevel = logging.DEBUG
+    server.setFormatter(FORMATTER)
 
-FILE.setLevel = logging.DEBUG
-FILE.setFormatter(FORMATTER)
-LOGGER.addHandler(FILE)
+    # user logs
+    # will be updated to be in user folder later
+    user_f = log_folder + 'user.log'
+    user = RotatingFileHandler(user_f, 'a', maxBytes=100000, backupCount=1)
+    user.setLevel = logging.ERROR
+    user.setFormatter(FORMATTER)
 
-LOGFILE_USER = LOG_FOLDER + "user.log"
-USER_FILE = RotatingFileHandler(LOGFILE_USER, 'a',\
-        maxBytes=100000, backupCount=1)
-USER_FILE.setLevel = logging.ERROR
-USER_FILE.setFormatter(FORMATTER)
-LOGGER.addHandler(USER_FILE)
-
-
-
-# console handler
-# STREAM_HANDLER = logging.StreamHandler()
-# STREAM_HANDLER.setLevel(logging.DEBUG)
-
-# LOGGER.addHandler(STREAM_HANDLER)
-
-
-#LOGGER.info('Hello')
-#LOGGER.warning('Testing %s', 'foo')
+    # add handlers
+    logger.addHandler(server)
+    logger.addHandler(user)
