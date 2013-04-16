fe9887217dd61eaeb3840b80058966f4bdb80d2b
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-16 18:24:02 +0200

    Missing ; in integration script

--- gateway_code/integration/integration_script.sh
+++ gateway_code/integration/integration_script.sh
@@ -2,9 +2,9 @@
 source /etc/profile;
 pkill socat;
 cd fit-dev/gateway_code_python/;
-rm -f coverage.xml nosetests.xml pylint.out
+rm -f coverage.xml nosetests.xml pylint.out;
 
 cp ~/python_missing_files/* . ;
 git pull;
 python setup.py nosetests -i='*integration/*';
-python setup.py lint --report | tee pylint.out
+python setup.py lint --report | tee pylint.out;
