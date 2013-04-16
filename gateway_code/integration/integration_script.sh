f9f47d123bbc09fb80a18ab7b2311235cc0d1490
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-16 17:14:21 +0200

    add a script to group all integration run commands

---
+++ gateway_code/integration/integration_script.sh
@@ -0,0 +1,11 @@
+
+source /etc/profile;
+pkill socat;
+cd fit-dev/gateway_code_python/;
+rm -f coverage.xml nosetests.xml pylint.out
+
+cp ~/python_missing_files/* . ;
+git reset --hard ;
+git pull;
+python setup.py nosetests -i='*integration/*';
+python setup.py lint --report | tee pylint.out
