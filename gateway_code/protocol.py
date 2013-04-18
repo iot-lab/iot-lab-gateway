7a320a1e92e7ffe6c3b3acc04c05330ff17b9b01
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-18 16:00:46 +0200

    catch correct error in pyserial, add get/reset_time commands

--- gateway_code/protocol.py
+++ gateway_code/protocol.py
@@ -2,6 +2,8 @@
 Protocol between gateway and control node
 """
 
+import struct
+
 PROTOCOL = {}
 
 
@@ -20,15 +22,24 @@ BATT = chr(0x0)
 DC   = chr(0x1)
 
 
-COMMAND = {'start': OPEN_NODE_START, 'stop': OPEN_NODE_STOP}
+
+# (res|g)et time
+RESET_OPEN_TIME = chr(0x72)
+
+GET_OPEN_TIME = chr(0x73)
+TIME_FACTOR = 32768
+
+
+
+ACK  = chr(0x0A)
+NACK = chr(0x02)
+
+
+COMMAND = {'start': OPEN_NODE_START, 'stop': OPEN_NODE_STOP, \
+        'reset_time': RESET_OPEN_TIME, 'get_time': GET_OPEN_TIME}
 ALIM    = {'batt': BATT, 'dc': DC,}
 
 
-
-
-# (res|g)et time
-# RESET_OPEN_TIME = chr(0x72)
-# GET_OPEN_TIME = chr(0x73)
 
 # MONITOR_POWER = chr(0x42)
 # MONITOR_RADIO = chr(0x44)
@@ -57,21 +68,73 @@ def _print_packet(info, data):
     debug_out += "'"
     print debug_out
 
+def _valid_result_command(packet, type, length):
+    """
+    Validate type of packet,
+    ACK/NACK
+    and length
+    """
+
+    ret = True
+    ret &= packet[0] == type
+    ret &= packet[1] == ACK
+    ret &= len(packet) == length
+
+    return ret
+
+def send_cmd(dispatcher, data):
+
+    _print_packet('Sent packet', data)
+    result = dispatcher.send_command(data)
+    _print_packet('Rec packet', result)
+
+    return result
 
 
-def start_stop(command, alim, dispatcher):
+
+
+def start_stop(dispatcher, command, alim):
 
     command_b = COMMAND[command]
     alim_b    = ALIM[alim]
 
     data = command_b + alim_b
 
-    _print_packet('Sent packet', data)
-    result = dispatcher.send_command(data)
-    _print_packet('Rec packet', result)
+    result = send_cmd(dispatcher, data)
+
+    # only type and ACK
+    ret = _valid_result_command(result, command_b, 2)
+    return ret
 
 
+def reset_time(dispatcher, command):
 
+    command_b = COMMAND[command]
+    data = command_b
+
+    result = send_cmd(dispatcher, data)
+
+    # only type and ACK
+    ret = _valid_result_command(result, command_b, 2)
+    return ret
+
+
+def get_time(dispatcher, command):
+    command_b = COMMAND[command]
+    data = command_b
+
+    result = send_cmd(dispatcher, data)
+
+    # type, ACK, and an unsigned long (4bytes)
+    ret = _valid_result_command(result, command_b, 6)
+    if not ret:
+        raise ValueError
+
+    data_result = result[2:]
+    control_time_tick = struct.unpack('!L', data_result)[0]
+    control_time_seconds = control_time_tick / float(TIME_FACTOR)
+
+    return control_time_seconds
 
 
 def parse_arguments(args):
@@ -88,6 +151,10 @@ def parse_arguments(args):
     parse_stop.add_argument('alim', choices=['dc', 'batt'], \
             help='Alimentation, dc => charging battery')
 
+    parse_reset  = sub.add_parser('reset_time', help='Reset control node time')
+
+    parse_get  = sub.add_parser('get_time', help='Get control node time')
+
 
     namespace = parser.parse_args(args)
     return namespace.command, namespace
@@ -97,6 +164,8 @@ def parse_arguments(args):
 _ARGS_COMMANDS = {
         'start': start_stop,
         'stop':  start_stop,
+        'reset_time': reset_time,
+        'get_time': get_time,
         }
 
 
@@ -118,7 +187,8 @@ def main(args):
 
 
 
-    _ARGS_COMMANDS[command](dispatcher = dis, **arguments.__dict__)
+    ret = _ARGS_COMMANDS[command](dispatcher = dis, **arguments.__dict__)
+    print '%s: %r' % (command, ret)
 
     rxtx.stop()
 
