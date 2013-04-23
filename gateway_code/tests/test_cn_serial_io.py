1480f8c6f394d85edf0f69291d3508d2bd9bbed0
Gaëtan Harter <gaetan.harter@inria.fr>
2013-04-23 15:41:53 +0200

    cover cn_serial_io at 100% with one test

--- gateway_code/tests/test_cn_serial_io.py
+++ gateway_code/tests/test_cn_serial_io.py
@@ -2,62 +2,17 @@
 # -*- coding:utf-8 -*-
 
 import mock
-import select
-
 
 from gateway_code import cn_serial_io
-import Queue
 import serial
-import sys
+import time
 import threading
 
 
 @mock.patch('serial.Serial')
-def test_rx_multiple_packets(serial_mock_class):
-
-    rx_queue = Queue.Queue(0)
-
-    # configure test
-    read_values = [cn_serial_io.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
-            [cn_serial_io.SYNC_BYTE, chr(1), 'x'] + \
-            [cn_serial_io.SYNC_BYTE, chr(2), cn_serial_io.SYNC_BYTE, cn_serial_io.SYNC_BYTE]
-    result_packets = ['abc', 'x', cn_serial_io.SYNC_BYTE + cn_serial_io.SYNC_BYTE]
-
-    unlock_test = threading.Event()
-
-    def read_one_val():
-        if read_values == []:
-            unlock_test.set()
-            raise select.error
-        return read_values.pop(0)
-
-    def read_mock(val=1):
-        res = ''
-        for _i in range(0, val):
-            res += read_one_val()
-        return res
-
-    def cb_packet_received(packet):
-        assert packet == result_packets.pop(0)
-
-    serial_mock = serial_mock_class.return_value
-    serial_mock.read.side_effect = read_mock
-
-
-    rxtx = cn_serial_io.RxTxSerial(cb_packet_received)
-    rxtx.start()
-
-    # wait until read finished
-    unlock_test.wait()
-
-    rxtx.stop()
-
-@mock.patch('serial.Serial')
 def test_rx_wih_invalid_start(serial_mock_class):
 
-    rx_queue = Queue.Queue(0)
-
-    # configure test
+    # configure test (invalid values at start)
     read_values = ['D', 'D', 'E','K','K'] + \
             [cn_serial_io.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
             [cn_serial_io.SYNC_BYTE, chr(1), 'x'] + \
@@ -68,22 +23,33 @@ def test_rx_wih_invalid_start(serial_mock_class):
 
     def read_one_val():
         if read_values == []:
-            unlock_test.set()
-            raise select.error
+            time.sleep(0.1)
+            return ''
         return read_values.pop(0)
 
     def read_mock(val=1):
-        res = ''
+        result = ''
         for _i in range(0, val):
-            res += read_one_val()
-        return res
+            ret = read_one_val()
+            result += ret
+            if ret == '':
+                break
+        if result == '':
+            unlock_test.set()
+        return result
 
     def cb_packet_received(packet):
         assert packet == result_packets.pop(0)
 
-
     serial_mock = serial_mock_class.return_value
     serial_mock.read.side_effect = read_mock
+
+    def serial_exception(val=1):
+        raise serial.SerialException
+    def mock_close():
+        serial_mock.read.side_effect = serial_exception
+    serial_mock.close.side_effect = mock_close
+
 
     rxtx = cn_serial_io.RxTxSerial(cb_packet_received)
     rxtx.start()
@@ -92,22 +58,5 @@ def test_rx_wih_invalid_start(serial_mock_class):
     unlock_test.wait()
 
     rxtx.stop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
