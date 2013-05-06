fe29560ebd91eee9b4b33122ec4cd07a950348c9
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-06 16:12:38 +0200

    Change test_cn_serial_io to unittest

--- gateway_code/tests/test_cn_serial_io.py
+++ gateway_code/tests/test_cn_serial_io.py
@@ -2,61 +2,65 @@
 # -*- coding:utf-8 -*-
 
 import mock
+import unittest
 
 from gateway_code import cn_serial_io
+from gateway_code.cn_serial_io import SYNC_BYTE
 import serial
 import time
 import threading
 
+class TestsCnSerialIo(unittest.TestCase):
 
-@mock.patch('serial.Serial')
-def test_rx_wih_invalid_start(serial_mock_class):
+    def setUp(self):
+        self.read_values = None
+        self.unlock_test = threading.Event()
 
-    # configure test (invalid values at start)
-    read_values = ['D', 'D', 'E','K','K'] + \
-            [cn_serial_io.SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
-            [cn_serial_io.SYNC_BYTE, chr(1), 'x'] + \
-            [cn_serial_io.SYNC_BYTE, chr(2), cn_serial_io.SYNC_BYTE, cn_serial_io.SYNC_BYTE]
-    result_packets = ['abc', 'x', cn_serial_io.SYNC_BYTE + cn_serial_io.SYNC_BYTE]
+        self.serial_patcher = mock.patch('serial.Serial')
+        serial_class_mock   = self.serial_patcher.start()
+        serial_mock         = serial_class_mock.return_value
 
-    unlock_test = threading.Event()
+        serial_mock.read.side_effect  = self._read_mock
+        def mock_close():
+            serial_mock.read.side_effect = serial.SerialException
+        serial_mock.close.side_effect = mock_close
 
-    def read_one_val():
-        if read_values == []:
-            time.sleep(0.1)
-            return ''
-        return read_values.pop(0)
+    def tearDown(self):
+        self.serial_patcher.stop()
 
-    def read_mock(val=1):
+    def _read_mock(self, val=1):
         result = ''
-        for _i in range(0, val):
-            ret = read_one_val()
-            result += ret
-            if ret == '':
-                break
-        if result == '':
-            unlock_test.set()
+        try:
+            for _i in range(0, val):
+                result += self.read_values.pop(0)
+        except IndexError: # last Item
+            time.sleep(0.1)
+            if result == '':
+                self.unlock_test.set()
         return result
 
-    def cb_packet_received(packet):
-        assert packet == result_packets.pop(0)
-
-    serial_mock = serial_mock_class.return_value
-    serial_mock.read.side_effect = read_mock
-
-    def serial_exception(val=1):
-        raise serial.SerialException
-    def mock_close():
-        serial_mock.read.side_effect = serial_exception
-    serial_mock.close.side_effect = mock_close
 
 
-    rxtx = cn_serial_io.RxTxSerial(cb_packet_received)
-    rxtx.start()
 
-    # wait until read finished
-    unlock_test.wait()
+    def test_rx_wih_invalid_start(self):
+        """ Test reception with invalid bytes at start """
 
-    rxtx.stop()
+        self.read_values = ['D', 'D', 'E','K','K'] + \
+                [SYNC_BYTE, chr(3), 'a', 'b', 'c'] + \
+                [SYNC_BYTE, chr(1), 'x'] + \
+                [SYNC_BYTE, chr(2), SYNC_BYTE, SYNC_BYTE]
+        self.result_packets = ['abc', 'x', SYNC_BYTE + SYNC_BYTE]
+
+        def cb_packet_received(pkt):
+            result = self.result_packets.pop(0)
+            self.assertEquals(pkt, result)
+
+        rxtx = cn_serial_io.RxTxSerial(cb_packet_received)
+        rxtx.start()
+
+        # wait until read finished
+        self.unlock_test.wait()
+
+        rxtx.stop()
 
 
