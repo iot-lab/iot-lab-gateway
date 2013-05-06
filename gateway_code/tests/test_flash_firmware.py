0feff3274d056e22cbf27719efd0fdd25bd58323
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-06 17:58:26 +0200

    Update flash_firmware to unittest. Add invalid config and firmware path test cases

--- gateway_code/tests/test_flash_firmware.py
+++ gateway_code/tests/test_flash_firmware.py
@@ -3,7 +3,6 @@
 
 import sys
 
-import re
 import mock
 import unittest
 from cStringIO import StringIO
@@ -17,21 +16,26 @@ CURRENT_DIR = dirname(abspath(__file__)) + '/'
 STATIC_DIR  = CURRENT_DIR + 'static/' # using the 'static' symbolic link
 
 from subprocess import PIPE
-@mock.patch('subprocess.Popen')
 @mock.patch('gateway_code.flash_firmware.config.STATIC_FILES_PATH', new=STATIC_DIR)
 class TestsFlashMethods(unittest.TestCase):
     """
     Tests flash_firmware methods
     """
-    def test_node_detection(self, mock_popen):
-        """
-        Test node detection
-        """
-        # config mock
-        popen = mock_popen.return_value
-        popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
-        popen.returncode = mock_ret = 0
+    def setUp(self):
+        self.popen_patcher = mock.patch('subprocess.Popen')
+        popen_class_mock = self.popen_patcher.start()
+        self.popen = popen_class_mock.return_value
 
+    def tearDown(self):
+        self.popen_patcher.stop()
+
+
+
+    def test_node_detection(self):
+        """ Test node detection """
+        # config mock
+        self.popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.returncode = mock_ret = 0
 
         # valid nodes
         for node in ('m3', 'gwt', 'a8'):
@@ -39,55 +43,49 @@ class TestsFlashMethods(unittest.TestCase):
             ret, out, err = flash_firmware.flash(node, filename)
 
         # invalid nodes
-        try:
-            node = 'INEXISTANT_ NODE'
-            ret, out, err = flash_firmware.flash(node, 'filename')
-        except:
-            pass
-        else:
-            assert 0, 'Non existant node not detected'
+        self.assertRaises(ValueError, flash_firmware.flash, 'INEXISTANT', '/dev/null')
 
 
-    def test_flash_OK(self, mock_popen):
+    def test_flash_OK(self):
         """
         Test with a flash with a successfull call
         """
 
         # config mock
-        popen = mock_popen.return_value
-        popen.returncode = mock_ret = 0
-        popen.communicate.return_value = \
-                (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.returncode = mock_ret = 0
 
         filename = STATIC_DIR + 'idle.elf'
         ret, out, err = flash_firmware.flash('m3', filename)
 
-        assert popen.communicate.call_count == 1
-        assert mock_out == out
-        assert mock_err == err
-        assert mock_ret == ret
+        self.assertEquals(self.popen.communicate.call_count, 1)
+        self.assertEquals((ret, out, err), (mock_ret, mock_out, mock_err))
 
 
-    def test_flash_Error(self, mock_popen):
+    def test_flash_Error(self):
         """
         Test with a flash with a unsuccessfull call
         """
-
-
-        popen = mock_popen.return_value
-        popen.returncode = mock_ret = 42
-        popen.communicate.return_value = \
+        self.popen.returncode = mock_ret = 42
+        self.popen.communicate.return_value = \
                 (mock_out, mock_err) = ("OUT_ERR", "ERR_ERR")
 
         filename = STATIC_DIR + 'idle.elf'
         ret, out, err = flash_firmware.flash('m3', filename)
 
-        assert popen.communicate.call_count == 1
+        self.assertEquals(self.popen.communicate.call_count, 1)
+        self.assertEquals((ret, out, err), (mock_ret, mock_out, mock_err))
 
-        assert mock_out == out
-        assert mock_err == err
-        assert mock_ret == ret
 
+class TestsFlashInvalidPaths(unittest.TestCase):
+    @mock.patch('gateway_code.reset.config.STATIC_FILES_PATH', new='/invalid/path/')
+    def test_invalid_config_file_path(self):
+        self.assertRaises(OSError, flash_firmware.flash, 'm3', '/dev/null')
+
+    @mock.patch('gateway_code.flash_firmware.config.STATIC_FILES_PATH', new=STATIC_DIR)
+    def test_invalid_firmware_path(self):
+        ret, out, err = flash_firmware.flash('m3', '/invalid/path')
+        self.assertNotEqual(ret, 0)
 
 
 
@@ -95,67 +93,34 @@ class TestsFlashMethods(unittest.TestCase):
 # Command line tests
 
 from cStringIO import StringIO
-captured_out = StringIO()
 captured_err = StringIO()
-@mock.patch('sys.stdout', captured_out)
 @mock.patch('sys.stderr', captured_err)
 class TestsCommandLineCalls(unittest.TestCase):
-    def test_error_no_arguments(self):
-        """
-        Running command line without arguments
-        """
-        try:
-            ret = flash_firmware.main(['flash_firmware.py'])
-        except SystemExit as ret:
-            assert ret.code != 0
-        else:
-            assert 0
 
-
-    def test_error_help(self):
-        """
-        Running command line with --help
-        """
-        try:
-            ret = flash_firmware.main(['flash_firmware.py', '-h'])
-        except SystemExit as ret:
-            assert ret.code == 0
-        else:
-            assert 0
-
-
+    def test_simple_args(self):
+        """ Running command line without arguments """
+        self.assertRaises(SystemExit, flash_firmware.main, ['flash_firmware.py'])
+        self.assertRaises(SystemExit, flash_firmware.main, ['flash_firmware.py', '-h'])
 
 
     @mock.patch('gateway_code.flash_firmware.flash')
     def test_normal_run(self, mock_fct):
-        """
-        Running command line with m3
-        """
+        """ Running command line with m3 """
         mock_fct.return_value = (0, 'OUTMessage', 'ErrorRunMessage')
-        try:
-            ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
-        except SystemExit, ret:
-            assert 0
-        else:
-            assert re.search('OK', captured_err.getvalue())
-            assert ret == 0
-        assert mock_fct.called
+        ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
+
+        self.assertEquals(ret, 0)
+        self.assertTrue(mock_fct.called)
 
 
     @mock.patch('gateway_code.flash_firmware.flash')
     def test_error_run(self, mock_fct):
-        """
-        Running command line with error during run
-        """
-        mock_fct.return_value = (42, 'OUT', 'ErrorRunMessage')
-        try:
-                ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
-        except SystemExit, ret:
-            assert 0
-        else:
-            err = captured_err.getvalue()
-            assert ret == 42
-        assert mock_fct.called
+        """ Running command line with error during run """
 
+        mock_fct.return_value = (42, 'OUT', 'ErrorRunMessage')
+        ret = flash_firmware.main(['flash_firmware.py', 'm3', '/dev/null'])
+
+        self.assertEquals(ret, 42)
+        self.assertTrue(mock_fct.called)
 
 
