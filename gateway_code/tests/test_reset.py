19982a4ed7fe15b902b4d27bc8513966bc171f41
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-06 17:42:01 +0200

    Update test_reset, move patching in setUp unittest method. Add invalid config_file_path case

--- gateway_code/tests/test_reset.py
+++ gateway_code/tests/test_reset.py
@@ -16,64 +16,67 @@ from gateway_code import reset
 CURRENT_DIR = dirname(abspath(__file__)) + '/'
 STATIC_DIR  = CURRENT_DIR + 'static/' # using the 'static' symbolic link
 
-from subprocess import PIPE
 @mock.patch('gateway_code.reset.config.STATIC_FILES_PATH', new=STATIC_DIR)
-@mock.patch('subprocess.Popen')
 class TestsResetMethods(unittest.TestCase):
     """
     Tests reset functions
     """
-    def test_node_detection(self, mock_popen):
-        """
-        Test node detection
-        """
+    def setUp(self):
+        self.popen_patcher = mock.patch('subprocess.Popen')
+        popen_class_mock = self.popen_patcher.start()
+        self.popen = popen_class_mock.return_value
+
+    def tearDown(self):
+        self.popen_patcher.stop()
+
+
+    def test_node_detection(self):
+        """ Test node detection """
         # config mock
 
-        popen = mock_popen.return_value
-        popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
-        popen.returncode = mock_ret = 0
+        self.popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.returncode               = mock_ret             = 0
 
         # valid nodes
         for node in ('m3', 'gwt', 'a8'):
             ret, out, err = reset.reset(node)
+            self.assertEquals(ret, mock_ret)
 
         # invalid nodes
-        node = 'INEXISTANT_ NODE'
-        self.assertRaises(ValueError, reset.reset, node)
+        self.assertRaises(ValueError, reset.reset, 'INEXISTANT_NODE')
 
 
-    def test_reset_OK(self, mock_popen):
+    def test_reset_OK(self):
         """
         successfull reset
         """
         # config mock
-        popen = mock_popen.return_value
-        popen.returncode = mock_ret = 0
-        popen.communicate.return_value = \
-                (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.communicate.return_value = (mock_out, mock_err) = ("OUT_MSG", "")
+        self.popen.returncode               = mock_ret             = 0
 
         ret, out, err = reset.reset('m3')
 
-        self.assertEquals(popen.communicate.call_count, 1)
+        self.assertEquals(self.popen.communicate.call_count, 1)
         self.assertEquals((ret, out, err), (mock_ret, mock_out, mock_err))
 
 
-    def test_reset_Error(self, mock_popen):
+    def test_reset_Error(self):
         """
         Test with a reset with a unsuccessfull call
         """
 
-        popen = mock_popen.return_value
-        popen.returncode = mock_ret = 42
-        popen.communicate.return_value = \
-                (mock_out, mock_err) = ("OUT_ERR", "ERR_ERR")
+        self.popen.communicate.return_value = (mock_out, mock_err) = ("OUT_ERR", "ERR_ERR")
+        self.popen.returncode               = mock_ret             = 42
 
-        filename = 'TEST_FILENAME'
         ret, out, err = reset.reset('m3')
 
-        self.assertEquals(popen.communicate.call_count, 1)
+        self.assertEquals(self.popen.communicate.call_count, 1)
         self.assertEquals((ret, out, err), (mock_ret, mock_out, mock_err))
 
+@mock.patch('gateway_code.reset.config.STATIC_FILES_PATH', new='/invalid/path/')
+class TestsResetInvalidConfigFilePath(unittest.TestCase):
+    def test_invalid_config_file_path(self):
+        self.assertRaises(OSError, reset.reset, 'm3')
 
 
 
@@ -81,13 +84,11 @@ class TestsResetMethods(unittest.TestCase):
 # Command line tests
 
 from cStringIO import StringIO
-captured_out = StringIO()
 captured_err = StringIO()
-@mock.patch('sys.stdout', captured_out)
 @mock.patch('sys.stderr', captured_err)
+@mock.patch('gateway_code.reset.reset')
 class TestsCommandLineCalls(unittest.TestCase):
 
-    @mock.patch('gateway_code.reset.reset')
     def test_normal_run(self, mock_fct):
         """
         Running command line with m3
@@ -99,7 +100,6 @@ class TestsCommandLineCalls(unittest.TestCase):
         self.assertTrue(mock_fct.called)
 
 
-    @mock.patch('gateway_code.reset.reset')
     def test_error_run(self, mock_fct):
         """
         Running command line with error during run
