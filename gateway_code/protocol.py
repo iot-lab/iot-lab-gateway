fb16d3b3225328cb6e576996afc45e9a617c4d1a
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-13 16:16:10 +0200

    Correct Line too long

--- gateway_code/protocol.py
+++ gateway_code/protocol.py
@@ -224,7 +224,8 @@ def config_consumption_command(sender, command, state, **kwargs):
     if state == 'start':
         consumption = gateway_code.profile.Consumption(**consumption_kwargs)
         # check correct configuration
-        if not (consumption.power or consumption.voltage or consumption.current):
+        if not (consumption.power or consumption.voltage or \
+                consumption.current):
             import sys
             print >> sys.stderr, "WARNING:",
             print >> sys.stderr, "No 'power', 'voltage' or 'current' flag set!",
