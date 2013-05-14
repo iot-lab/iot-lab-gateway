dc6f0478a643dcc3251b693fec8f66ce6199ea88
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-14 19:05:04 +0200

    Change protocol to a class to store current configuration, prepare code for configuration_ack like measures

--- gateway_code/protocol.py
+++ gateway_code/protocol.py
@@ -1,18 +1,22 @@
+# -*- coding:utf-8 -*-
 """
 Protocol between gateway and control node
+
+Packet send format
+SYNC_BYTE | LEN | TYPE | -?-?-?-?-
+
 """
 
-import struct
-import Queue
 import gateway_code.profile
-import datetime
-
-PROTOCOL = {}
+from datetime import timedelta, datetime
 
 
-# Packet send format
-# SYNC_BYTE | LEN | TYPE | -?-?-?-?-
 SYNC_BYTE = chr(0x80)
+
+ACK  = chr(0x0A)
+NACK = chr(0x02)
+
+TIME_FACTOR = 32768
 
 
 
@@ -21,35 +25,22 @@ SYNC_BYTE = chr(0x80)
 #
 
 # Commands
-# start stop
 TYPE_CMD_OPEN_NODE_START = chr(0x70)
 TYPE_CMD_OPEN_NODE_STOP  = chr(0x71)
-# reset time
 TYPE_CMD_RESET_TIME = chr(0x72)
-# Config RADIO
-# TYPE_CMD_CONFIG_RADIO =
-
-# Radio Noise
-# TYPE_CMD_CONFIG_RADIO_NOISE = ? -> no monitoring values
-# fake sensor
-# TYPE_CMD_CONFIG_FAKE_SENSOR = ?
 
 # Config measures
 TYPE_CMD_CONFIG_MEASURES_CONSUMPTION = chr(0x79)
 # TYPE_CMD_CONFIG_MEASURES_RADIO = ?
 # TYPE_CMD_CONFIG_MEASURES_SNIFFER = ?
+# TYPE_CMD_CONFIG_RADIO_NOISE = ? -> no monitoring values
+# TYPE_CMD_CONFIG_FAKE_SENSOR = ?
 
 
-
-
+# defines
 
 BATT = chr(0x0)
 DC   = chr(0x1)
-
-TIME_FACTOR = 32768
-
-ACK  = chr(0x0A)
-NACK = chr(0x02)
 
 
 #
@@ -91,6 +82,9 @@ POWER_SOURCE = {
         '5V'   : CONSUMPTION_5V,
         'BATT' : CONSUMPTION_BATT,
         }
+
+POWER_SOURCE_VAL = dict([(POWER_SOURCE[key], key) \
+        for key in POWER_SOURCE])
 
 
 # CONFIG BYTE == [ PERIOD | AVERAGE << 4 | ENABLE(BIT7) ]
@@ -136,11 +130,37 @@ COMMAND = {'start': TYPE_CMD_OPEN_NODE_START, 'stop': TYPE_CMD_OPEN_NODE_STOP, \
 ALIM    = {'battery': BATT, 'dc': DC,}
 
 
-def _print_packet(info, data):
+
+
+
+# store list of measures and associated unpack strings for each config
+CONSUMPTION_DECODE_VALUES = {}
+for conf in range(1, 1 << 3):
+    _measures_list = ['t'] + \
+            (['p'] if conf & CONSUMPTION_POWER else []) + \
+            (['v'] if conf & CONSUMPTION_VOLTAGE else []) + \
+            (['c'] if conf & CONSUMPTION_CURRENT else [])
+    _unpack_str = '!L' + ('f' * (len(_measures_list) - 1))
+    # 4 bytes for time + 4bytes per value (float)
+    _measures_len = 4 + 4 * (len(_measures_list) -1)
+    CONSUMPTION_DECODE_VALUES[conf] = \
+            (_measures_list, _unpack_str, _measures_len)
+
+
+
+
+def _str_packet(info, data):
     """
     Debug function that prints a packet
+
+
+    >>> _str_packet('Msg', chr(0x20) + chr(0xFF))
+    "Msg: '20 FF '"
+
+    >>> _str_packet('Info', None)
+    "Info: 'None'"
+
     """
-    import sys
     debug_out = info + ": '"
     if data is None:
         debug_out += str(None)
@@ -149,69 +169,217 @@ def _print_packet(info, data):
             debug_out += '%02X ' % ord(i)
 
     debug_out += "'"
-    print >> sys.stderr, debug_out
+    return debug_out
 
 def _valid_result_command(packet, pkt_type, length):
     """
     Validate type of packet,
     ACK/NACK
     and length
+
+    >>> _valid_result_command(chr(0x42) + ACK, chr(0x42), 2)
+    0
+
+    >>> _valid_result_command(None, '', 42)
+    1
+    >>> _valid_result_command(chr(0x42) + NACK, chr(0x42), 2)
+    1
+    >>> _valid_result_command(chr(0x42) + ACK, chr(0x42), 5)
+    1
+    >>> _valid_result_command(chr(0x42) + ACK, chr(0x66), 2)
+    1
+
     """
 
     if packet is None:
-        return False
-    ret_b = True
-    ret_b &= (packet[0] == pkt_type)
-    ret_b &= (packet[1] == ACK)
-    ret_b &= (len(packet) == length)
+        ret_b = False
+    else:
+        ret_b = True
+        ret_b &= (packet[0] == pkt_type)
+        ret_b &= (packet[1] == ACK)
+        ret_b &= (len(packet) == length)
 
     ret   = 0 if ret_b else 1
     return ret
 
-def send_cmd(sender, data):
+
+class Protocol(object):
     """
-    Send a command to the control node and wait for it's answer.
-    """
-
-    _print_packet('Sent packet', data)
-    result = sender(data)
-    _print_packet('Rec packet', result)
-
-    return result
-
-
-
-
-def start_stop(sender, command, alim):
-    """
-    Start or stop open node via the control node and use given alim.
+    Class protocol containing current configuration for packets reception
     """
 
-    command_b = COMMAND[command]
-    alim_b    = ALIM[alim]
-    data      = command_b + alim_b
+    def __init__(self, sender):
 
-    result = send_cmd(sender, data)
+        self.sender   = sender
+        self.time     = None
+        self.new_time = None
 
-    ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
-    return ret
+        self.conso_conf = {}
+
+        self.measures_decode = {
+                chr(0xFF): self.decode_consumption_pkt,
+                }
+
+    def send_cmd(self, data):
+        """
+        Send a command to the control node and wait for it's answer.
+        """
+
+        import sys
+        print >> sys.stderr, _str_packet('Sent packet', data)
+        result = self.sender(data)
+        print >> sys.stderr, _str_packet('Rec packet', data)
+
+        return result
 
 
-def reset_time(sender, command):
-    """
-    Reset the time reference on the control node at 0
-    """
+    def start_stop(self, command, alim):
+        """
+        Start or stop open node via the control node and use given alim.
+        """
 
-    command_b = COMMAND[command]
-    data      = command_b
+        command_b = COMMAND[command]
+        alim_b    = ALIM[alim]
+        data      = command_b + alim_b
 
-    result = send_cmd(sender, data)
+        result = self.send_cmd(data)
 
-    ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
-    return ret
+        ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
+        return ret
+
+    def reset_time(self, command):
+        """
+        Reset the time reference on the control node at 0
+        """
+
+        command_b = COMMAND[command]
+        data      = command_b
+
+        result = self.send_cmd(data)
+
+        ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
+        return ret
 
 
-def config_consumption_command(sender, command, state, **kwargs):
+    def config_consumption(self, consumption=None):
+        """
+        Configure consumption measures
+
+        :param consumption: the consumption object
+        :type consumption: gateway_code.profile.Consumption
+        """
+
+        command_b     = COMMAND['consumption']
+        measures_flag = 0
+        config_flag   = 0
+
+        if (consumption is None):
+            # stop consumption measures
+            config_flag |= INA226_STATE['stop']
+        elif not (consumption.power or consumption.voltage or \
+                consumption.current): #pragma: no cover
+            # no values asked, disable
+            # stop consumption measures
+            config_flag |= INA226_STATE['stop']
+        else:
+            # start and configure consumption
+            for measure in 'power', 'voltage', 'current':
+                if getattr(consumption, measure):
+                    measures_flag |= POWER_MEASURES[measure]
+            measures_flag |= POWER_SOURCE[consumption.source]
+
+            config_flag |= INA226_PERIOD[consumption.period]
+            config_flag |= INA226_AVERAGE[consumption.average] << 4
+            config_flag |= INA226_STATE['start']
+
+
+        data = command_b + chr(measures_flag) + chr(config_flag)
+        result = self.send_cmd(data)
+
+        ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
+        return ret
+
+
+
+
+    def decode_measure_packet(self, pkt):
+        """
+        Generic measure decoding function.
+        Calls the appropriate function for each type.
+        """
+        measure_type = pkt[0]
+        try:
+            ret = self.measures_decode[measure_type](pkt)
+        except KeyError: #pragma: no cover
+            from sys import stderr # TODO replace with logger
+            print >> stderr, 'Uknown measure packet: %02X' % ord(measure_type)
+            ret = None
+
+        return ret
+
+
+    def update_consumption_config(self, config_byte):
+        """
+        Update the current configuration for consumption decoding
+        """
+
+        self.conso_conf['source'] = POWER_SOURCE_VAL[config_byte & 0x70]
+
+        self.conso_conf['measures'] = ['t'] + \
+            (['p'] if config_byte & CONSUMPTION_POWER else []) + \
+            (['v'] if config_byte & CONSUMPTION_VOLTAGE else []) + \
+            (['c'] if config_byte & CONSUMPTION_CURRENT else [])
+        _num_measures = len(self.conso_conf['measures']) - 1 # without time
+
+
+        # Time = uint, values = float
+        self.conso_conf['len']        = 4    + _num_measures * 4
+        self.conso_conf['unpack_str'] = '!L' + _num_measures * 'f'
+
+
+    def decode_consumption_pkt(self, pkt):
+        """
+        Extract measures stored in pkt.
+        """
+        from struct import unpack
+
+        header_size = 3
+        config = ord(pkt[1])
+        count  = ord(pkt[2])
+
+        # decode current frame content
+        # TODO remove after ack update
+        self.update_consumption_config(config)
+
+
+        assert len(pkt) == header_size + count * self.conso_conf['len']
+        # extract list of raw measures from payload
+        chunks = [pkt[start:start + self.conso_conf['len']] for start in \
+                range(header_size, len(pkt), self.conso_conf['len'])]
+        assert len(chunks) == count
+
+
+        # extract the 'count' measures
+        all_measures = []
+        for raw_measure in chunks:
+            measures = list(unpack(self.conso_conf['unpack_str'], raw_measure))
+            # convert tick to seconds + time reference
+            measures[0] = self.time + timedelta(seconds=\
+                    (measures[0] / float(TIME_FACTOR)))
+            all_measures.append(measures)
+
+        return {self.conso_conf['source']: \
+                (self.conso_conf['measures'], all_measures)}
+
+
+
+
+#
+# CLI dedicated functions
+#
+
+def config_consumption_command(protocol_obj, command, state, \
+        **kwargs): #pragma: no cover
     """
     Configure consumption measures.
     Function called when used from command line.
@@ -231,142 +399,22 @@ def config_consumption_command(sender, command, state, **kwargs):
             print >> sys.stderr, "No 'power', 'voltage' or 'current' flag set!",
             print >> sys.stderr, "Consumption disabled\n"
 
-    ret = config_consumption(sender, consumption)
-    return ret
-
-
-def config_consumption(sender, consumption=None):
-    """
-    Configure consumption measures
-
-    :param sender: function that sends command
-    :param consumption: the consumption object
-    :type consumption: gateway_code.profile.Consumption
-    """
-
-    command_b     = COMMAND['consumption']
-    measures_flag = 0
-    config_flag   = 0
-
-    if (consumption is None):
-        # stop consumption measures
-        config_flag |= INA226_STATE['stop']
-    elif not (consumption.power or consumption.voltage or consumption.current):
-        # no values asked, disable
-        # stop consumption measures
-        config_flag |= INA226_STATE['stop']
-    else:
-        # start and configure consumption
-        measures_flag |= POWER_MEASURES['power']   if consumption.power   else 0
-        measures_flag |= POWER_MEASURES['voltage'] if consumption.voltage else 0
-        measures_flag |= POWER_MEASURES['current'] if consumption.current else 0
-        measures_flag |= POWER_SOURCE[consumption.source]
-
-        config_flag |= INA226_PERIOD[consumption.period]
-        config_flag |= INA226_AVERAGE[consumption.average] << 4
-        config_flag |= INA226_STATE['start']
-
-
-    data = command_b + chr(measures_flag) + chr(config_flag)
-    result = send_cmd(sender, data)
-
-    ret = _valid_result_command(result, command_b, 2) # type and [N]ACK
+    ret = protocol_obj.config_consumption(consumption)
     return ret
 
 
 
-
-CONSUMPTION_TUPLE = \
-        ((CONSUMPTION_POWER, 'p'), \
-        (CONSUMPTION_VOLTAGE, 'v'), \
-        (CONSUMPTION_CURRENT, 'c'))
-
-
-# store list of measures and associated unpack strings for each config
-CONSUMPTION_DECODE_VALUES = {}
-for conf in range(1, 1 << 3):
-    _measures_list = ['t'] + \
-            (['p'] if conf & CONSUMPTION_POWER else []) + \
-            (['v'] if conf & CONSUMPTION_VOLTAGE else []) + \
-            (['c'] if conf & CONSUMPTION_CURRENT else [])
-    _unpack_str = '!L' + ('f' * (len(_measures_list) - 1))
-    # 4 bytes for time + 4bytes per value (float)
-    _measures_len = 4 + 4 * (len(_measures_list) -1)
-    CONSUMPTION_DECODE_VALUES[conf] = \
-            (_measures_list, _unpack_str, _measures_len)
-
-CONSUMPTION_SOURCE_VALUES = dict([(POWER_SOURCE[key], key) \
-        for key in POWER_SOURCE])
-
-
-def decode_consumption_pkt(pkt, ref_time):
-    """
-    Extract measures stored in pkt.
-    """
-
-    header_size = 3
-    unpack_str  = '!'
-    config = ord(pkt[1])
-    count  = ord(pkt[2])
-
-    # decode current frame content
-    power_source                      = CONSUMPTION_SOURCE_VALUES[config & 0x70]
-    values, unpack_str, measures_size = CONSUMPTION_DECODE_VALUES[config & 0x7]
-
-    assert len(pkt) == header_size + count * measures_size
-    # extract list of raw measures from payload
-    chunks = [pkt[start:start + measures_size] for start in \
-            range(header_size, len(pkt), measures_size)]
-    assert len(chunks) == count
-
-    # extract the 'count' measures
-    all_measures = []
-    for raw_measure in chunks:
-        measures = list(struct.unpack(unpack_str, raw_measure))
-        # convert tick to seconds + time reference
-        measures[0] = ref_time + \
-                datetime.timedelta(seconds=(measures[0] / float(TIME_FACTOR)))
-        all_measures.append(measures)
-
-    return {power_source: (values, all_measures)}
-
-
-
-
-MEASURES_DECODE = {
-        TYPE_MEASURES_CONSUMPTION: decode_consumption_pkt,
-        }
-
-def decode_measure_packet(pkt, ref_time):
-    """
-    Generic measure decoding function.
-    Calls the appropriate function for each type.
-    """
-    fct = MEASURES_DECODE.get(pkt[0], None)
-
-    if fct is None:
-        import sys
-        print >> sys.stderr, 'Uknown measure packet: %02X' % ord(pkt[0])
-        ret = None
-    else:
-        ret = fct(pkt, ref_time)
-
-    return ret
-
-
-def _listen(queue, command):
+def _listen(queue, protocol): #pragma: no cover
     """
     Listen for incoming measures packets.
     Debug function, to be called from Command line.
     """
-    _ = command
-    ref_time = datetime.datetime.fromtimestamp(0)
-
+    import Queue
     while True:
         try:
             raw_pkt = queue.get(True, timeout=1)
-            _print_packet('MEASURE_PKT', raw_pkt)
-            measure_pkt = decode_measure_packet(raw_pkt, ref_time)
+            print _str_packet('MEASURE_PKT', raw_pkt)
+            measure_pkt = protocol.decode_measure_packet(raw_pkt)
             print measure_pkt
         except Queue.Empty:
             pass
@@ -375,8 +423,7 @@ def _listen(queue, command):
 
 
 
-
-def parse_arguments(args):
+def parse_arguments(args): #pragma: no cover
     """
     Parse the arguments, accept following commands
         start, stop, reset_time,
@@ -419,29 +466,43 @@ def parse_arguments(args):
 
 
     # listen to sensor measures
-    _parse_listen_measures = \
+    parse_listen = \
             sub.add_parser('listen', help='listen for measures packets')
+    parse_listen.add_argument(\
+            'consumption_source', choices=sorted(POWER_SOURCE.keys()))
+    parse_listen.add_argument(\
+            '-p', '--power',   help = 'Measure power', action="store_true")
+    parse_listen.add_argument(\
+            '-v', '--voltage', help = 'Measure voltage', action="store_true")
+    parse_listen.add_argument(\
+            '-c', '--current', help = 'Measure current', action="store_true")
+
+
+
+
 
     namespace = parser.parse_args(args)
     print namespace
     return namespace.command, namespace
 
 
+
+
 _ARGS_COMMANDS = {
-        'start': start_stop,
-        'stop':  start_stop,
-        'reset_time': reset_time,
-        'consumption':config_consumption_command,
-        'listen': _listen,
+        'start'       : 'start_stop',
+        'stop'        : 'start_stop',
+        'reset_time'  : 'reset_time',
+        'consumption' : 'config_consumption_command',
         }
 
 
-def main(args):
+def main(args): #pragma: no cover
     """
     Main command line function
     """
     from gateway_code import dispatch, cn_serial_io
     import atexit
+    import Queue
 
     command, arguments = parse_arguments(args[1:])
 
@@ -451,15 +512,27 @@ def main(args):
     rxtx = cn_serial_io.RxTxSerial(dis.cb_dispatcher)
     dis.io_write = rxtx.write
 
-
     rxtx.start()
     atexit.register(rxtx.stop) # execute even if there is an exception
 
 
     if command == 'listen':
         try:
-            ret = _ARGS_COMMANDS[command](queue = dis.measures_queue, \
-                    **arguments.__dict__)
+            protocol      = Protocol(dis.send_command)
+            protocol.time = datetime.now()
+
+            # configure reception for consumption
+            measures_flag = 0
+            for measure in 'power', 'voltage', 'current':
+                if getattr(arguments, measure):
+                    measures_flag |= POWER_MEASURES[measure]
+            measures_flag |= POWER_SOURCE[arguments.consumption_source]
+            protocol.update_consumption_config(measures_flag)
+
+            # configure reception for radio
+
+            ret = _listen(queue = dis.measures_queue, protocol = protocol)
+
         except KeyboardInterrupt:
             print 'Got Ctrl+C Stopping'
             ret = 0
