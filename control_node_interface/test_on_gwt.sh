f825eedcfed5821072909e7fa49a74a709d0ca1c
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-28 11:38:04 +0200

    Make before testing, quiet mode for clean and scp

--- control_node_interface/test_on_gwt.sh
+++ control_node_interface/test_on_gwt.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-git clean -fdX
-scp -r ../control_node_interface/ gw-m3:
+make || exit 42
+
+git clean -fdX -q
+scp -r -q ../control_node_interface/ gw-m3:
 ssh gw-m3 'cd control_node_interface; make run'
 
