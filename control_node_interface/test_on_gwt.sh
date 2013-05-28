56bede65bd8b37bc74a5588ce23f9dfbd4b42eb1
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-28 17:00:50 +0200

    Configure control node before running program

--- control_node_interface/test_on_gwt.sh
+++ control_node_interface/test_on_gwt.sh
@@ -6,5 +6,6 @@ make || exit 42
 
 git clean -fdX -q
 scp -r -q ../control_node_interface/ gw-m3:
+ssh -t gw-m3 'cd fit-dev/gateway_code_python/; ./control_node_interaction consumption start 3.3V --average 1 --period 140us -p -v -c'
 ssh -t gw-m3 'cd control_node_interface; make run'
 
