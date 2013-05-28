cf6a837de0babdd976144897e40086a277652080
Gaëtan Harter <gaetan.harter@inria.fr>
2013-05-28 16:03:25 +0200

    Adding CFLAGS

--- control_node_interface/test_on_gwt.sh
+++ control_node_interface/test_on_gwt.sh
@@ -6,5 +6,5 @@ make || exit 42
 
 git clean -fdX -q
 scp -r -q ../control_node_interface/ gw-m3:
-ssh gw-m3 'cd control_node_interface; make run'
+ssh -t gw-m3 'cd control_node_interface; make run'
 
