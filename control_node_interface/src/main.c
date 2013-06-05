d6b7f25f9f88f4f6eb82e5fce9a5bd70c9de37ec
Gaëtan Harter <gaetan.harter@inria.fr>
2013-06-05 20:25:14 +0200

    Parse stdin and write packet content

--- control_node_interface/src/main.c
+++ control_node_interface/src/main.c
@@ -1,8 +1,16 @@
+#include <unistd.h>
 
+#include "command_reader.h"
 
 int main(int argc, char *argv[])
 {
         (void) argc;
         (void) argv;
+
+
+        int serial_fd = 0;
+        command_reader_start(serial_fd);
+        pause();
+
         return 0;
 }
