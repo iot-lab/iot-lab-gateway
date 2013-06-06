4c73b4ec9f926f2f58a6c75f5d8690b79e9d7b4a
Gaëtan Harter <gaetan.harter@inria.fr>
2013-06-06 10:57:54 +0200

    Compiling on gateway

--- control_node_interface/test_on_gwt.sh
+++ control_node_interface/test_on_gwt.sh
@@ -3,9 +3,10 @@
 set -x
 
 make || exit 42
+make clean
 
 git clean -fdX -q
 scp -r -q ../control_node_interface/ gw-m3:
 ssh -t gw-m3 'cd fit-dev/gateway_code_python/; ./control_node_interaction consumption start 3.3V --average 1 --period 140us -p -v -c'
-ssh -t gw-m3 'cd control_node_interface; make run'
+ssh -t gw-m3 'cd control_node_interface; make clean run'
 
