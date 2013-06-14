857c74c163e88760a4e84efe769f4000ec5056cd
Gaëtan Harter <gaetan.harter@inria.fr>
2013-06-14 18:58:37 +0200

    [GW-CODE] Adding unit-test suite for control_node_serial C code.

---
+++ control_node_serial/tests/utils_test.c
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include "utils.c"
+
+
+/* Mock EXTERNAL dependency functions 
+ * with function definition
+ * */
+
+
+
+/* Test values */
+struct dict_entry example_d[] = {
+        {"key1", 1},
+        {"key2", 2},
+        {"key3", 3},
+        {NULL, 0},
+};
+
+
+TEST(dict_get_val_tests, get_valid_values)
+{
+        uint8_t value = 0;
+        int ret;
+
+        ret = get_val("key1", example_d, &value);
+        ASSERT_EQ(0, ret);
+        ASSERT_EQ((uint8_t) 1, value);
+
+        ret = get_val("key3", example_d, &value);
+        ASSERT_EQ(0, ret);
+        ASSERT_EQ((uint8_t) 3, value);
+}
+
+TEST(dict_get_val_tests, get_unknown_value)
+{
+        uint8_t value = 0;
+        int ret;
+
+        ret = get_val("unkown_key", example_d, &value);
+        ASSERT_NE(0, ret);
+
+        ret = get_val(NULL, example_d, &value);
+        ASSERT_NE(0, ret);
+}
+
