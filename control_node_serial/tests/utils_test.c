d32c8ebe09e5834bca5b61f369f5c26a27837a1f
Gaëtan Harter <gaetan.harter@inria.fr>
2013-06-18 18:22:10 +0200

    [GW-CODE] add coverage on utils C file

--- control_node_serial/tests/utils_test.c
+++ control_node_serial/tests/utils_test.c
@@ -30,7 +30,6 @@ TEST(dict_get_val_tests, get_valid_values)
         ASSERT_EQ(0, ret);
         ASSERT_EQ((uint8_t) 3, value);
 }
-
 TEST(dict_get_val_tests, get_unknown_value)
 {
         uint8_t value = 0;
@@ -38,8 +37,30 @@ TEST(dict_get_val_tests, get_unknown_value)
 
         ret = get_val("unkown_key", example_d, &value);
         ASSERT_NE(0, ret);
-
         ret = get_val(NULL, example_d, &value);
+        ASSERT_NE(0, ret);
+}
+
+
+TEST(dict_get_key_tests, get_valid_keys)
+{
+        char *key = NULL;
+        int ret;
+
+        ret = get_key(1, example_d, &key);
+        ASSERT_EQ(0, ret);
+        ASSERT_STREQ("key1", key);
+
+        ret = get_key(3, example_d, &key);
+        ASSERT_EQ(0, ret);
+        ASSERT_STREQ("key3", key);
+}
+TEST(dict_get_key_tests, get_uknown_keys)
+{
+        char *key = NULL;
+        int ret;
+
+        ret = get_key(42, example_d, &key);
         ASSERT_NE(0, ret);
 }
 
