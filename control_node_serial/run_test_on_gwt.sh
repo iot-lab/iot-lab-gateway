d8bcd95b5d438d7837aeb02970aaa6f98209b81c
Gaëtan Harter <gaetan.harter@inria.fr>
2013-07-01 17:55:58 +0200

    [GW-CODE] Correct tty configuration. Now handles correctly received frames from control node

--- control_node_serial/run_test_on_gwt.sh
+++ control_node_serial/run_test_on_gwt.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-TARGET=gateway-m3
 set -x
+TARGET=gateway-m3
+if [[ $# -eq 1 ]]; then TARGET="$1"; fi
+
 QUIET='-q'
 
 make || exit 42
@@ -9,6 +11,6 @@ make clean
 
 git clean -fdX $QUIET
 scp -r $QUIET ../control_node_serial/ ${TARGET}:
-ssh -t ${TARGET} 'source /etc/profile; cd fit-dev/gateway_code_python/; ./flash_firmware gwt static/control_node.elf'
+ssh -t ${TARGET} 'source /etc/profile; cd fit-dev/gateway_code_python/; ./bin/scripts/flash_firmware gwt static/control_node.elf'
 ssh -t ${TARGET} 'cd control_node_serial;  make clean all && { python -u ./examples/send_commands.py examples/test_commands | make run; }'
 
