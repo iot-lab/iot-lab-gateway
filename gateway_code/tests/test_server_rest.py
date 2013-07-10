e7ca9fa74ed5a1711e3356e9c5432006dc175fe6
Gaëtan Harter <gaetan.harter@inria.fr>
2013-07-10 10:29:40 +0200

    [GW-CODE] Mock the path to board_type config file in tests

--- gateway_code/tests/test_server_rest.py
+++ gateway_code/tests/test_server_rest.py
@@ -15,9 +15,14 @@ import os
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_DIR  = CURRENT_DIR + '/static/' # using the 'static' symbolic link
 
+# Patch to find idle.elf
+# Patch to find control_node.elf at startup
+# Patch to find '/var/log/config/board_type' -> tests/config_m3/board_type
 @patch('gateway_code.flash_firmware.config.STATIC_FILES_PATH', new=STATIC_DIR)
 @patch('gateway_code.gateway_manager.CONTROL_NODE_FIRMWARE', \
         STATIC_DIR + 'control_node.elf')
+@patch('gateway_code.gateway_manager.config.GATEWAY_CONFIG_PATH', \
+        os.path.dirname(os.path.abspath(__file__)) + '/config_m3/')
 class TestServerRest(unittest.TestCase):
     """
     Cover functions uncovered by unit tests
