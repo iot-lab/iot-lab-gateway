96e3b158e20f91f15e130af3674bdfd76676494e
Gaëtan Harter <gaetan.harter@inria.fr>
2013-07-10 17:14:44 +0200

    [GW-CODE] Remove decode function from main.c for testing purpose

--- control_node_serial/src/main.c
+++ control_node_serial/src/main.c
@@ -1,45 +1,21 @@
 #include <unistd.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 
-#include "constants.h"
 #include "common.h"
 #include "serial.h"
 #include "command_reader.h"
-#include "measures_handler.h"
+#include "decode.h"
 
-static char *tty_path = "/dev/ttyFITECO_GWT";
-
-static void decode_pkt(struct pkt *current_pkt)
-{
-        uint8_t pkt_type = current_pkt->data[0];
-
-#if DEBUG
-        for (uint8_t i=0; i < current_pkt->len; i++) {
-                DEBUG_PRINT(" %02X", current_pkt->data[i]);
-        }
-        DEBUG_PRINT("\n");
-#endif
-
-        if ((pkt_type & MEASURES_FRAME_MASK) == MEASURES_FRAME_MASK) {
-                // send to measures packets handler
-                handle_measure_pkt(current_pkt->data, current_pkt->len);
-        } else {
-                // answer commands
-                write_answer(current_pkt->data, current_pkt->len);
-        }
-}
 
 int main(int argc, char *argv[])
 {
-        int serial_fd = 0;
+        int serial_fd  = 0;
+        char *tty_path = "/dev/ttyFITECO_GWT";
+
 
         // Get tty_path from arguments if available
-        // allow simple running and more advanced from application and tests
         if (argc == 2)
                 tty_path = argv[1];
-
 
         if ((serial_fd = configure_tty(tty_path)) <= 0) {
                 fprintf(LOG, "ERROR: Could not open and configure TTY %s\n", tty_path);
@@ -48,7 +24,6 @@ int main(int argc, char *argv[])
         }
 
         command_reader_start(serial_fd);
-
         start_listening(serial_fd, decode_pkt);
 
         return 0;
