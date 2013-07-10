cecb1aa8d259defc16fb6bdf300940c10224f708
Gaëtan Harter <gaetan.harter@inria.fr>
2013-07-10 16:46:03 +0200

    [GW-CODE] PEP8 check of config.py

--- gateway_code/config.py
+++ gateway_code/config.py
@@ -6,19 +6,21 @@ Common configuration for openocd scripts
 """
 
 
-STATIC_FILES_PATH   = '/var/lib/gateway_code/'
+STATIC_FILES_PATH = '/var/lib/gateway_code/'
 GATEWAY_CONFIG_PATH = '/var/local/config/'
 
-NODES_CFG   = {
-        'gwt': { 'openocd_cfg_file':'fiteco-gwt.cfg',
-            'tty':'/dev/ttyFITECO_GWT', 'baudrate':500000},
-        'm3': { 'openocd_cfg_file':'fiteco-m3.cfg',
-            'tty':'/dev/ttyFITECO_M3',  'baudrate':500000},
-        'a8': { 'openocd_cfg_file':'fiteco-a8.cfg',
-            'tty':'/dev/ttyFITECO_A8',  'baudrate':500000},
+NODES_CFG = {
+    'gwt': {'openocd_cfg_file': 'fiteco-gwt.cfg',
+            'tty': '/dev/ttyFITECO_GWT',
+            'baudrate': 500000},
+    'm3': {'openocd_cfg_file': 'fiteco-m3.cfg',
+           'tty': '/dev/ttyFITECO_M3',
+           'baudrate': 500000},
+    'a8': {'openocd_cfg_file': 'fiteco-a8.cfg',
+           'tty': '/dev/ttyFITECO_A8',
+           'baudrate': 500000},
     }
 
 NODES = NODES_CFG.keys()
 
 CONTROL_NODE_SERIAL_INTERFACE = 'control_node_serial_interface'
-
