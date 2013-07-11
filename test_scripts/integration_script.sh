09831cb10a31c4fbf18e8a7d5af4609e2d090328
Gaëtan Harter <gaetan.harter@inria.fr>
2013-07-11 17:30:36 +0200

    [GW-CODE] Add pep8 command to setup.py

--- test_scripts/integration_script.sh
+++ test_scripts/integration_script.sh
@@ -14,6 +14,7 @@ rm -f coverage.xml nosetests.xml pylint.out
 
 # cp ~/python_missing_files/* .   # missing files for 'requests' and 'pylint'
 cp ~/python_missing_files/contextlib.py .   # missing for pylint
+cp ~/python_missing_files/runpy.py .   # missing for python -mpep8
 
 git pull
 
@@ -22,4 +23,4 @@ set -e
 # run as the same user as bottle server
 su  www-data   -c "source /etc/profile; PATH=./control_node_serial/:$PATH python setup.py build_cn_serial integration_tests $@"
 python setup.py jenkins_lint
-python -mpep8 | tee pep8.out
+python setup.py pep8 | tee pep8.out
