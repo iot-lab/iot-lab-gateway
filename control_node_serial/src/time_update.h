c89ab4205d6c4f1fd4aed0a73cb5a04b51ef5725
Gaëtan Harter <gaetan.harter@inria.fr>
2013-08-06 15:02:11 +0200

    [GW-CODE] Update time reference when ACK is received, instead of when command is sent, corrects architecture bypassing

--- control_node_serial/src/time_update.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIME_UPDATE_H
-#define TIME_UPDATE_H
-
-#include <sys/time.h>
-extern struct timeval new_time_ref;
-
-#endif // TIME_UPDATE_H
--- control_node_serial/tests/serial_test.c
+++ control_node_serial/tests/serial_test.c
@@ -4,7 +4,6 @@
 char print_buff[2048];
 #define fprintf(stream, ...)  snprintf(print_buff, sizeof(print_buff), __VA_ARGS__)
 
-#include "time_update.c"
 #include "serial.c"
 
 
