90ad66e42dcc04553e77e3c6cd28cb702fb709a3
Gaëtan Harter <gaetan.harter@inria.fr>
2013-08-07 10:48:49 +0200

    [GW-CODE] Change default logging for user.log. Pep8 and pylint on setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #! /usr/share/env python
 # -*- coding:utf-8 -*-
 
+"""
+setup.py deployement script
+
+Install command:
+
+    python setup.py install
+
+Tests commands:
+
+    python setup.py tests
+    python setup.py integration
+    python setup.py test_roomba
+
+Pylint and pep8 checker:
+
+    python setup.py lint
+    python setup.py pep8
+
+
+"""
+
 from setuptools import setup, Command
 from setuptools.command.install import install
 
@@ -9,27 +30,36 @@ import re
 import subprocess
 
 from gateway_code import config
+# Disable: R0904 - Too many public methods
+# Disable: W0201 - Attribute '%s' defined outside __init__
+# pylint: disable=R0904,W0201
 
 
 STATIC_FILES_PATH = config.STATIC_FILES_PATH
-STATIC_FILES      = ['static/' + item for item in os.listdir('static')]
-INIT_SCRIPT       = ('/etc/init.d/', ['bin/init_script/gateway-server-daemon'])
-DATA              = [(STATIC_FILES_PATH, STATIC_FILES), INIT_SCRIPT]
+STATIC_FILES = ['static/' + item for item in os.listdir('static')]
+INIT_SCRIPT = ('/etc/init.d/', ['bin/init_script/gateway-server-daemon'])
 
-SCRIPTS           = ['control_node_serial/control_node_serial_interface']
-SCRIPTS          += ['bin/scripts/' + el for el in os.listdir('bin/scripts')]
+DATA = [(STATIC_FILES_PATH, STATIC_FILES), INIT_SCRIPT]
 
-INSTALL_REQUIRES  = ['argparse', 'bottle', 'paste', 'recordtype', 'pyserial']
-TESTS_REQUIRES    = ['nose>=1.0', 'pylint', 'nosexcover', 'mock', 'pep8']
+SCRIPTS = ['control_node_serial/' + config.CONTROL_NODE_SERIAL_INTERFACE]
+SCRIPTS += ['bin/scripts/' + el for el in os.listdir('bin/scripts')]
+
+INSTALL_REQUIRES = ['argparse', 'bottle', 'paste', 'recordtype', 'pyserial']
+TESTS_REQUIRES = ['nose>=1.0', 'pylint', 'nosexcover', 'mock', 'pep8']
 
 # unload 'gateway_code.config'
 # either it's not included in the coverage report...
-import sys; del sys.modules['gateway_code.config']
+import sys
+del sys.modules['gateway_code.config']
 
 
 class _Tee(object):
-    def __init__(self, name, mode):
-        self.file = open(name, mode)
+    """
+    File object which mimic 'tee' linux command behaviour
+    It writes to file_path and stdout
+    """
+    def __init__(self, file_path, mode):
+        self.file = open(file_path, mode)
         self.stdout = sys.stdout
         sys.stdout = self
 
@@ -38,6 +68,9 @@ class _Tee(object):
         self.file.close()
 
     def write(self, data):
+        """
+        Write data to stdout and file
+        """
         self.file.write(data)
         self.stdout.write(data)
 
@@ -48,8 +81,10 @@ class _Tee(object):
         pass
 
 
-
 def build_c_executable():
+    """
+    Build control node serial interface
+    """
     saved_path = os.getcwd()
     os.chdir('control_node_serial')
     try:
@@ -58,9 +93,12 @@ def build_c_executable():
         exit(1)
     os.chdir(saved_path)
 
-def setup_permissions():
 
-    import stat
+def setup_permissions():
+    """
+    Setup file permissions on scripts
+    Add www-data user to dialout group
+    """
     # set init script executable
     init_script_path = INIT_SCRIPT[0] + os.path.basename(INIT_SCRIPT[1][0])
     mode = 0755
@@ -71,18 +109,30 @@ def setup_permissions():
     subprocess.check_call(usermod_args)
 
 
-
 class BuildSerial(Command):
+    """
+    Build control node serial interface command
+    """
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         build_c_executable()
 
 
 class Install(install):
+    """
+    Install command
+
+    * Build control node serial
+    * install python code
+    * correct file permissions
+    """
     def run(self):
         build_c_executable()
         install.run(self)
@@ -90,9 +140,12 @@ class Install(install):
 
 
 class Lint(Command):
+    """
+    Pylint command
+    """
     user_options = [
-            ('report', 'r', "print errors and report"),
-            ('outfile=', 'o', "duplicate output to file")]
+        ('report', 'r', "print errors and report"),
+        ('outfile=', 'o', "duplicate output to file")]
 
     def initialize_options(self):
         self.report = False
@@ -104,13 +157,17 @@ class Lint(Command):
     def run(self):
         from pylint import lint
         lint_args = self.report_opt
-        lint_args += ['--rcfile=pylint.rc', 'gateway_code/', 'roomba/']
+        lint_args += ['--rcfile=pylint.rc', 'gateway_code/', 'roomba/',
+                      'setup.py']
 
         with _Tee(self.outfile, 'w'):
             lint.Run(lint_args, exit=False)
 
 
 class Pep8(Command):
+    """
+    Pep8 command
+    """
     user_options = [('outfile=', 'o', "duplicate output to file")]
 
     def initialize_options(self):
@@ -124,9 +181,14 @@ class Pep8(Command):
         import pep8
         sys.argv = ['./pep8.py', 'gateway_code/', 'roomba/']
         with _Tee(self.outfile, 'w'):
-            pep8._main()
+            pep8._main()  # pylint: disable=W0212
+
 
 def _add_path_to_coverage_xml():
+    """
+    Add source path in coverage report,
+    relativ to parent folder of git repository
+    """
     current_folder = os.path.abspath('.')
     base_folder = os.path.abspath('../..') + '/'
 
@@ -139,11 +201,16 @@ def _add_path_to_coverage_xml():
 
     subprocess.check_call(args)
 
+
 class Tests(Command):
+    """
+    Run unit tests, pylint and pep8
+    """
     user_options = []
 
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
 
@@ -158,11 +225,16 @@ class Tests(Command):
         except subprocess.CalledProcessError:
             exit(1)
 
+
 class TestsRoomba(Command):
+    """
+    Run roomba specific tests, should be run on a node with a robot conected
+    """
     user_options = []
 
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
 
@@ -176,40 +248,41 @@ class TestsRoomba(Command):
             exit(1)
 
 
-def add_path_to_coverage_xml():
-    from os import path
-    os.path.abspath()
-
-    os.getcwd()
-
-
-
-
-
 class IntegrationTests(Command):
+    """
+    Run unit tests, pylint and pep8, and integration tests.
+    Should be run on a gateway
+    """
     user_options = []
 
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
 
     @staticmethod
     def cleanup_workspace():
+        """
+        Remove old scripts output.
+        Correct file permissions to be used with www-data
+        """
         import stat
         # remove outfiles
-        for file in ('coverage.xml', 'nosetests.xml', 'pylint.out', 'pep8.out'):
-            if os.path.exists(file):
-                os.remove(file)
+        outfiles = ('coverage.xml', 'nosetests.xml', 'pylint.out', 'pep8.out')
+        _ = [os.remove(_f) for _f in outfiles if os.path.exists(_f)]
 
         # chmod o+x script_dir
         script_dir = os.path.dirname(os.path.abspath(__file__))
         mode = os.stat(script_dir).st_mode | stat.S_IWOTH
         os.chmod(script_dir, mode)
 
-
     @staticmethod
     def popen_as_user(user):
+        """
+        Allow running popen as another user.
+        Also sets all groups user belongs to.
+        """
         import pwd
         import grp
 
@@ -221,25 +294,26 @@ class IntegrationTests(Command):
         user_home_dir = pw_record.pw_dir
 
         env = os.environ.copy()
-        env[ 'HOME'     ]  = user_home_dir
-        env[ 'LOGNAME'  ]  = user_name
-        env[ 'USER'     ]  = user_name
+        env['LOGNAME'] = user_name
+        env['USER'] = user_name
+        env['HOME'] = user_home_dir
 
         def preexec_set_uid_gid():
+            """
+            preexec fct run by popen to update user and groups
+            """
             groups = [group.gr_gid for group in grp.getgrall()
                       if user in group.gr_mem]
-            os.setgroups(groups) # add all groups of user (for 'dialout')
+            os.setgroups(groups)  # add all groups of user (for 'dialout')
             os.setgid(user_gid)
-            os.setuid(user_uid) #setuid after setgid to have permissions
+            os.setuid(user_uid)  # setuid after setgid to have permissions
 
         return preexec_set_uid_gid, env
-
 
     def run(self):
         args = ['python', 'setup.py']
 
         self.cleanup_workspace()
-
 
         try:
             subprocess.check_call(args + ['build_cn_serial'])
@@ -256,24 +330,23 @@ class IntegrationTests(Command):
             exit(1)
 
 
-
 setup(name='gateway_code',
-        version='0.3',
-        description='Linux Gateway code',
-        author='SensLAB Team',
-        author_email='admin@senslab.info',
-        url='http://www.senslab.info',
-        packages = ['gateway_code', 'roomba'],
-        scripts = SCRIPTS,
-        data_files = DATA,
+      version='0.3',
+      description='Linux Gateway code',
+      author='SensLAB Team',
+      author_email='admin@senslab.info',
+      url='http://www.senslab.info',
+      packages=['gateway_code', 'roomba'],
+      scripts=SCRIPTS,
+      data_files=DATA,
 
-        cmdclass = {'lint': Lint, 'install': Install, \
-                'build_cn_serial': BuildSerial, 'pep8': Pep8,
+      cmdclass={'install': Install,
+                'build_cn_serial': BuildSerial,
+                'lint': Lint,
+                'pep8': Pep8,
                 'tests': Tests,
                 'integration': IntegrationTests,
-                'test_roomba':TestsRoomba},
-        install_requires = INSTALL_REQUIRES,
-        setup_requires = TESTS_REQUIRES + INSTALL_REQUIRES,
-        )
-
-
+                'test_roomba': TestsRoomba},
+      install_requires=INSTALL_REQUIRES,
+      setup_requires=TESTS_REQUIRES + INSTALL_REQUIRES,
+      )
