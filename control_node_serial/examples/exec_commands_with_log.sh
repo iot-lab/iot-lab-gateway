eea88d7b89589a132444fd2aef3647f31d9aa1db
Gaëtan Harter <gaetan.harter@inria.fr>
2013-08-23 13:40:23 +0200

    [Linux A8] Add tty as parameter to autotest script and accept stdin as input

---
+++ control_node_serial/examples/exec_commands_with_log.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -x
+
+test_file=$1
+
+stdout_file="output.log"
+stderr_file="debug.log"
+
+make -C ..
+python -u send_commands.py  $test_file | ../control_node_serial_interface > >(tee $stdout_file) 2> >(tee $stderr_file >&2)
