e4d2f6c87d931f77e13af50f9741b692466f2f83
Gaëtan Harter <gaetan.harter@inria.fr>
2013-09-12 14:46:41 +0200

    [GW-CODE] Add full oml code with ruby mp generation

--- control_node_serial/src/oml_measures.h
+++ control_node_serial/src/oml_measures.h
@@ -1,11 +1,11 @@
 #ifndef OML_MEASURES_H
 #define OML_MEASURES_H
 
-int oml_measures_init(char *oml_config_file_path);
+int oml_measures_start(char *oml_config_file_path);
 int oml_measures_stop(void);
 
-void oml_measures_consumption(void);
-void oml_measures_radio(void);
+void oml_measures_consumption(uint64_t timestamp_s, uint32_t timestamp_us, double current, double voltage, double power);
+void oml_measures_radio(uint64_t timestamp_s, uint32_t timestamp_us, int32_t rssi, int32_t lqi);
 
 
 #endif // OML_MEASURES_H
--- control_node_serial/tests/oml_measures_test.c
+++ control_node_serial/tests/oml_measures_test.c
@@ -1,8 +1,50 @@
 #include <gtest/gtest.h>
 
+#include "mock_fprintf.h"  // include before other includes
+
 #include <unistd.h>
+#include <sys/stat.h>
+
+#include <oml2/omlc.h>
+
+
+#define omlc_init omlc_init_mock
+#define omlc_start omlc_start_mock
+int omlc_init(const char* name, int* argc_ptr, const char** argv, o_log_fn logger);
+int omlc_start(void);
 #include "oml_measures.c"
 
+#undef omlc_init
+#undef omlc_start
+
+
+int omlc_init_do_mock = 0;
+int omlc_init_mock_ret = 0;
+int omlc_init_mock(const char* name, int* argc_ptr, const char** argv, o_log_fn logger)
+{
+        if (omlc_init_do_mock)
+                return omlc_init_mock_ret;
+        else
+                return omlc_init(name, argc_ptr, argv, logger);
+}
+
+int omlc_start_do_mock = 0;
+int omlc_start_mock_ret = 0;
+int omlc_start_mock()
+{
+        if (omlc_start_do_mock)
+                return omlc_start_mock_ret;
+        else
+                return omlc_start();
+}
+
+
+static int file_size(char *file_path)
+{
+        struct stat st;
+        stat(file_path, &st);
+        return st.st_size;
+}
 
 
 /* Mock EXTERNAL dependency functions
@@ -17,28 +59,69 @@
 
 TEST(test_oml_measures, init_and_stop)
 {
+        omlc_init_do_mock = 0;
+        omlc_start_do_mock = 0;
+
+
         int ret_init;
         int ret_stop;
-        ret_init = oml_measures_init(OML_CONFIG_PATH);
+        ret_init = oml_measures_start(OML_CONFIG_PATH);
         ret_stop = oml_measures_stop();
 
         ASSERT_EQ(0, ret_init);
         ASSERT_EQ(0, ret_stop);
         ASSERT_NE(-1, access(CONSUMPTION_FILE, F_OK));
         ASSERT_NE(-1, access(RADIO_FILE, F_OK));
+        unlink(CONSUMPTION_FILE);
+        unlink(RADIO_FILE);
+
 
 }
 
 TEST(test_oml_measures, init_and_stop_with_measures_print)
 {
+        omlc_init_do_mock = 0;
+        omlc_start_do_mock = 0;
+
         int ret_init;
         int ret_stop;
-        ret_init = oml_measures_init(OML_CONFIG_PATH);
+        ret_init = oml_measures_start(OML_CONFIG_PATH);
+
+        // add measures
+        oml_measures_consumption(42, 69, 12.34, 3.3, 40.72);
+        oml_measures_radio(42, 70, -20, 0);
+        oml_measures_consumption(43, 69, 12.34, 3.3, 40.72);
+        oml_measures_radio(43, 70, -20, 0);
 
         ret_stop = oml_measures_stop();
 
+
         ASSERT_EQ(0, ret_init);
         ASSERT_EQ(0, ret_stop);
+        // file not empty
+        ASSERT_NE(0, file_size(CONSUMPTION_FILE));
+        ASSERT_NE(0, file_size(RADIO_FILE));
+
+        unlink(CONSUMPTION_FILE);
+        unlink(RADIO_FILE);
 
 }
 
+TEST(test_oml_measures, init_and_stop_error_cases)
+{
+        omlc_init_do_mock = 1;
+        omlc_start_do_mock = 1;
+        int ret_init;
+        int ret_stop;
+
+        // error on omlc_init
+        omlc_init_mock_ret = -1;
+        ret_init = oml_measures_start(OML_CONFIG_PATH);
+        ASSERT_EQ(-1, ret_init);
+
+        // error on omlc_start
+        omlc_init_do_mock = 0;
+        omlc_start_mock_ret = -1;
+        ret_init = oml_measures_start(OML_CONFIG_PATH);
+        ASSERT_EQ(-1, ret_init);
+}
