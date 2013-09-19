c532a18a06e693031691de100a78d3d2aa746817
Gaëtan Harter <gaetan.harter@inria.fr>
2013-09-19 10:21:42 +0200

    [GW-CODE] Add -d on example for control_node_serial to get measures printed

--- control_node_serial/examples/exec_commands_with_log.sh
+++ control_node_serial/examples/exec_commands_with_log.sh
@@ -7,4 +7,4 @@ stdout_file="output.log"
 stderr_file="debug.log"
 
 make -C ..
-python -u send_commands.py  $test_file | ../control_node_serial_interface > >(tee $stdout_file) 2> >(tee $stderr_file >&2)
+python -u send_commands.py  $test_file | ../control_node_serial_interface -d > >(tee $stdout_file) 2> >(tee $stderr_file >&2)
