2ea0b6afa8b69c162266e08c9e33375f897f6150
Gaëtan Harter <gaetan.harter@inria.fr>
2013-09-25 15:51:45 +0200

    Presentation inges: ajout d'images et des dot pour les generer

--- generate_dot_archi.py
+++ generate_dot_archi.py
@@ -51,19 +51,60 @@ for current in source_files:
                 print >> sys.stderr, current_file, dep_file
                 node_str = NODE_LINK % (current_file, dep_file)
                 if dep_file in ('config', 'common'):
-                    node_str += ' [style=dotted]'
+                    node_str += ' [style=invis, constraint=false]'
+                elif dep_file in ('gateway_logging', ):
+                    node_str += ' [constraint=false, minlen=4]'
+                else:
+                    if ('gateway_validation', 'profile') != (current_file, dep_file):
+                        node_str += ' [headport="w"]'
+
                 nodes_assoc_list.append(node_str)
 
-nodes_assoc_list.append(NODE_LINK % ('common', 'config') + ' [style=invis]')
-nodes_assoc_list.append(NODE_LINK % ('config', 'common') + ' [style=invis]')
+nodes_assoc_list.append(NODE_LINK % ('profile', 'open_node_validation_interface') + ' [style=invis]')
+nodes_assoc_list.append(NODE_LINK % ('config', 'gateway_logging') + ' [style=invis]')
+
+nodes_assoc_list.append(NODE_LINK % ('gateway_roomba', 'roomba/roomba.py'))
+nodes_assoc_list.append(NODE_LINK % ('control_node_interface', 'control_node_serial'))
 
 
 print HEADER,
 for file_path in source_files:
     file_name = splitext(basename(file_path))[0]
     print NODE % (file_name, file_name),
+
+print '    "control_node_serial" [shape="Mrecord"];'
 for node_ass in nodes_assoc_list:
     print node_ass
+
+# Add clusters to be more readable
+print """    subgraph cluster_config {
+        config;
+        common;
+        style = invis;
+    }
+"""
+print """    subgraph cluster_control_node {
+        label = control_node;
+        control_node_interface;
+        protocol_cn;
+        style = dotted;
+    }
+"""
+print """    subgraph cluster_auto_tests {
+        label = auto_tests;
+        gateway_validation;
+        open_node_validation_interface;
+        open_a8_interface;
+        style = dotted;
+    }
+"""
+print """    subgraph cluster_subprocess {
+        label = open_node;
+        openocd_cmd;
+        serial_redirection;
+        style = dotted;
+    }
+"""
 print FOOTER,
 
 
