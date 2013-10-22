40b3b187d14ed4d501d620cd5462ecfbe87190a9
Gaëtan Harter <gaetan.harter@inria.fr>
2013-10-22 14:33:17 +0200

    Add a test for dispatch.send_command

--- gateway_code/tests/test_dispatch.py
+++ gateway_code/tests/test_dispatch.py
@@ -55,4 +55,50 @@ def test_cb_dispatcher(serial_mock_class):
     rxtx.stop()
 
 
+@mock.patch('serial.Serial')
+def test_cb_dispatcher_send_cmd(serial_mock_class):
+
+    unlock_test = threading.Event()
+    unlock_rx_thread = threading.Event()
+
+    # configure test
+    read_values = [SYNC_BYTE, chr(4), chr(0xFF) + 'a' + 'b' + 'c'] + \
+            [SYNC_BYTE, chr(4), chr(0x42) +  'def'] + \
+            [SYNC_BYTE, chr(2), chr(0xFF), 'Q']
+
+    result_omlpackets = [chr(0xFF) + 'abc'] + [chr(0xFF) + 'Q']
+    result_cnpackets = [chr(0x42) + 'def']
+
+    oml_queue = Queue.Queue(0)
+    dis = dispatch.Dispatch(oml_queue)
+    rxtx = cn_serial_io.RxTxSerial(dis.cb_dispatcher)
+
+    dis.io_write = rxtx.write
+
+
+    global first_run
+    first_run = True
+
+    # mock serial.read method
+    def read_mock():
+        unlock_rx_thread.wait()
+        if read_values == []:
+            unlock_test.set()
+            raise ValueError
+        return read_values.pop(0)
+    serial_mock = serial_mock_class.return_value
+    serial_mock.read.side_effect = read_mock
+    serial_mock.write.side_effect = lambda x: unlock_rx_thread.set()
+
+
+    rxtx.start()
+
+    cn_answer = dis.send_command('A')
+
+    # wait until read finished
+    unlock_test.wait()
+
+    assert cn_answer == result_cnpackets[0]
+
+    rxtx.stop()
 
