3c9abded2049457c323453883908718b579ef587
Gaëtan Harter <gaetan.harter@inria.fr>
2013-12-04 15:40:16 +0100

    [GW-CODE] Syntax Error ... U_U

--- gateway_code/open_a8_interface.py
+++ gateway_code/open_a8_interface.py
@@ -57,10 +57,10 @@ class OpenA8Connection(object):
 
         import datetime
         import sys
-        sys.stdout.write("time before %r" % datetime.datetime.now()
+        sys.stdout.write("time before %r" % datetime.datetime.now())
         # timeout 4 minutes -> max 5 minutes for all tests
         ret = a8_serial.expect(' login: ', timeout=300)
-        sys.stdout.write("time after %r" % datetime.datetime.now()
+        sys.stdout.write("time after %r" % datetime.datetime.now())
         if not ret:
             raise A8ConnectionError("Open node didn't booted: %r" % ret)
 
