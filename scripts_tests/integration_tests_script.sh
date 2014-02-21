aa32a7440929511cdf3974cbe27aadd413f1289c
Gaëtan Harter <gaetan.harter@inria.fr>
2014-02-21 13:29:08 +0100

    [GW-CODE][Tests] Make integration tests work

--- scripts_tests/integration_tests_script.sh
+++ scripts_tests/integration_tests_script.sh
@@ -7,46 +7,35 @@ then
 fi
 
 
-HOSTNAME="$1"
-SSH_CONFIG="$(readlink -e ~/.ssh/config)"
-
 if [[ $# == 2 ]]
 then
     SSH_CONFIG="$(readlink -e $2)"
+else
+    SSH_CONFIG="$(readlink -e ~/.ssh/config)"
 fi
+HOSTNAME="$1"
 
-
-
-SCRIPT_DIR="$(dirname $0)"
-SRC_DIR="$(readlink -e $SCRIPT_DIR/..)"
-
+SRC_DIR="$(readlink -e $(dirname $0)/..)"
 DEST="/tmp/fit-dev/"
 
+
 set -x
-
-
 
 # update gateway_code_python to host with www-data:www-data as owner
 rsync -e "ssh -F $SSH_CONFIG" -av --delete   $SRC_DIR   $HOSTNAME:$DEST
 ssh -F $SSH_CONFIG   $HOSTNAME "chown -R www-data:www-data $DEST"
 
+#############
+# Run tests #
+#############
 
-#
-# Run tests
-#
-
-# run integration tests
-ssh -F $SSH_CONFIG   $HOSTNAME "\
-    su www-data -c 'python $DEST/gateway_code_python/setup.py tests'"
-    #su www-data -c 'python $DEST/gateway_code_python/setup.py integration'"
+# run python tests
+ssh -F $SSH_CONFIG   $HOSTNAME "su www-data -c '\
+    source /etc/profile; \
+    python $DEST/gateway_code_python/setup.py integration'"
 
 # run control_node_serial tests
 ssh -F $SSH_CONFIG   $HOSTNAME "\
     source /etc/profile; \
-    make -C $DEST/gateway_code_python/control_node_serial realclean test \
-    coverage"
-
-
-
-
+    make -C $DEST/gateway_code_python/control_node_serial realclean coverage"
 
