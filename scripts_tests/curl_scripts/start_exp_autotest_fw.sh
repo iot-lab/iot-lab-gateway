2cd6d89ac8797996bfbcfbbbb1af8917d4d19bf1
Gaëtan Harter <gaetan.harter@inria.fr>
2014-02-24 14:03:37 +0100

    [GW-CODE] Scripts tests, set config via ssh

--- scripts_tests/curl_scripts/start_exp_autotest_fw.sh
+++ scripts_tests/curl_scripts/start_exp_autotest_fw.sh
@@ -1,7 +1,8 @@
 #! /bin/bash -x
 
 
-mkdir -p ../../../clochette/.senslab/123/consumption
-mkdir -p ../../../clochette/.senslab/123/radio
+ssh gateway 'rm   -rf /iotlab/users/clochette/.senslab/123/
+             mkdir -p /iotlab/users/clochette/.senslab/123/consumption
+             mkdir -p /iotlab/users/clochette/.senslab/123/radio'
 
 curl -X POST -H "Content-Type: multipart/form-data" http://gateway:8080/exp/start/123/clochette -F "firmware=@../../static/m3_autotest.elf" -F "profile=@consumption.json"; echo
