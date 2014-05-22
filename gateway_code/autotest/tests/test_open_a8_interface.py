bb64c181cf3629da4af74f24fa712257e122e707
Gaëtan Harter <gaetan.harter@inria.fr>
2014-05-22 14:04:18 +0200

    Add coverage for open_a8_interface exception

---
+++ gateway_code/autotest/tests/test_open_a8_interface.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+import unittest
+from gateway_code.autotest import open_a8_interface
+
+
+
+
+class TestA8ConnectionError(unittest.TestCase):
+    def test_connection_error(self):
+        error = open_a8_interface.A8ConnectionError("value", "err_msg")
+        self.assertEquals("'value' : 'err_msg'", str(error))
+
