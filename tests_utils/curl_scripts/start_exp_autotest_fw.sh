fa1a4e9adcd9926e3ba4cafa53ed61d6153646af
Romain Jayles <romain@jayles.net>
2015-06-23 16:25:32 +0200

    modify script

--- tests_utils/curl_scripts/start_exp_autotest_fw.sh
+++ tests_utils/curl_scripts/start_exp_autotest_fw.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -x
 
 
-ssh gateway 'rm   -rf /iotlab/users/clochette/.senslab/123/
-             mkdir -p /iotlab/users/clochette/.senslab/123/consumption
-             mkdir -p /iotlab/users/clochette/.senslab/123/radio'
+#ssh gateway 'rm   -rf /iotlab/users/clochette/.senslab/123/
+ #            mkdir -p /iotlab/users/clochette/.senslab/123/consumption
+  #           mkdir -p /iotlab/users/clochette/.senslab/123/radio'
 
-curl -X POST -H "Content-Type: multipart/form-data" http://gateway:8080/exp/start/123/clochette -F "firmware=@../../static/m3_autotest.elf" -F "profile=@consumption.json"; echo
+curl -X POST -H "Content-Type: multipart/form-data" http://localhost:8080/exp/start/123/test -F "firmware=@/tmp/iot-lab-gateway/gateway_code/static/fox_autotest.elf" -F "profile=@consumption.json"; echo
