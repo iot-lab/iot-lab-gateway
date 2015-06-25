b803cdbb9afc0e15653c9e5422b609aa08f0fb14
Romain Jayles <romain@jayles.net>
2015-06-25 13:40:13 +0200

    test with waitTTY modified

--- gateway_code/open_node.py
+++ gateway_code/open_node.py
@@ -44,10 +44,11 @@ class NodeFox(object):
     def teardown(self):
         """ Stop serial redirection and flash idle firmware """
         ret_val = 0
-        ret_val += common.wait_tty(self.TTY, LOGGER, timeout=5)
+        
         # cleanup debugger before flashing
         ret_val += self.debug_stop()
         ret_val += self.serial_redirection.stop()
+        ret_val += common.wait_tty(self.TTY, LOGGER, timeout=10)
         ret_val += self.flash(None)
         return ret_val
 
