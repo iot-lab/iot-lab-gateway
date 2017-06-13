84b59f7034b8cf6e9c724dc907e13afed1ca42ad
croussel <croussel@polytech-lille.net>
2017-06-13 15:23:39 +0200

    ADD Docker-run.sh options now enables a gateway_code mounting possibility.
    Changes the run script to provide both possibilities of mounting or not a gateway-code repository.

--- Docker-run.sh
+++ Docker-run.sh
@@ -1,9 +1,22 @@
+#!/bin/sh
+
+OPT_DIR=""
+if [ ! -z $1 ] ; then
+  if [ -d $1 ] ; then
+    echo "$1 mounted as gateway_code repository\n"
+    OPT_DIR="-v $1:/usr/local/lib/python2.7/dist-packages/gateway_code-2.4.0-py2.7-linux-x86_64.egg/gateway_code"
+  else
+    echo "Please use a valid path.\n"
+    exit 0
+  fi
+fi
 docker run \
-  -v /home/cedric/POLYTECH/stage/Environnement/Work/iot-lab-gateway/:/setup_dir/iot-lab-gateway/ \
+  $OPT_DIR \
   --rm \
   -p 8080:8080 \
   -p 20000:20000 \
   --name "gateway_test" \
   --privileged \
   -v /dev:/dev \
+  -d \
   iot_gateway
