66f3baf0dc1be692fd49239042af6d6733a57c96
croussel <croussel@polytech-lille.net>
2017-07-03 14:52:15 +0200

    ADD Docker-run.sh now has usage upgrade
    Adds Usage,-x, --x=, etc.

--- Docker-run.sh
+++ Docker-run.sh
@@ -1,16 +1,42 @@
 #!/bin/sh
 
+Options=$@
 OPT_DIR=""
-BOARD_TYPE="firefly"
+BOARD_TYPE="M3"
 CONTROL_NODE_TYPE="no"
 HOSTNAME="custom-123"
 
-while getopts "c:b:h:" opt ; do
+
+_usage(){
+  cat <<EOF
+Docker-run.sh $Options
+$*
+Usage : Docker-run.sh <[options]>
+Options :
+  -b --board=...          Set node board type (default : $BOARD_TYPE)
+  -h --host=...           Set hostname (default : $HOSTNAME)
+  -c --node-type=...      Set control_node_type (default : $CONTROL_NODE_TYPE)
+  --help                  Show this message
+EOF
+}
+
+
+while getopts "c:b:h:d:-:" opt ; do
 case $opt in
     b) BOARD_TYPE=$OPTARG; shift 2;;
     h) HOSTNAME=$OPTARG; shift 2 ;;
     c) CONTROL_NODE_TYPE=$OPTARG; shift 2 ;;
-
+    d) DEVICE=$OPTARG; shift 2 ;;
+    -) LONG_OPTARG="${OPTARG#*=}"
+      case $OPTARG in
+        board=?* ) BOARD_TYPE=$LONG_OPTARG; shift 1 ;;
+        board* ) echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+        host=?* ) HOSTNAME=$LONG_OPTARG; shift 1 ;;
+        host* ) echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+        node-type=?* ) CONTROL_NODE_TYPE=$LONG_OPTARG; shift 1 ;;
+        node-type* ) echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+        help ) _usage ; exit 0 ;;
+      esac
 esac
 done
 
@@ -23,6 +49,13 @@ if [ ! -z $1 ] ; then
     exit 0
   fi
 fi
+
+cat<<EOF
+Used config :
+  BOARD_TYPE=$BOARD_TYPE, HOSTNAME=$HOSTNAME, CONTROL_NODE_TYPE=$CONTROL_NODE_TYPE
+  Montage : $OPT_DIR
+EOF
+
 docker run \
   $OPT_DIR \
   --rm \
