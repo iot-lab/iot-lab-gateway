4b50e8864d022b1ea8b1d102183da6a4905449ae
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-08-28 16:48:27 +0200

    open_nodes: let NodeStlinkBase base node inherit NodeOpenOCDBase

--- gateway_code/open_nodes/common/node_st_link.py
+++ gateway_code/open_nodes/common/node_st_link.py
@@ -20,21 +20,11 @@
 # knowledge of the CeCILL license and that you accept its terms.
 
 """ Open Node based on ST-Link programmer implementation """
-import logging
 
-import serial
-
-from gateway_code.nodes import OpenNodeBase
-from gateway_code import common
-from gateway_code.common import logger_call
-
-from gateway_code.utils.openocd import OpenOCD
-from gateway_code.utils.serial_redirection import SerialRedirection
-
-LOGGER = logging.getLogger('gateway_code')
+from gateway_code.open_nodes.common.node_openocd import NodeOpenOCDBase
 
 
-class NodeStLinkBase(OpenNodeBase):
+class NodeStLinkBase(NodeOpenOCDBase):
     # pylint:disable=no-member
     """ Open node STM32 St-Link based board implementation """
 
@@ -43,89 +33,3 @@ class NodeStLinkBase(OpenNodeBase):
     BAUDRATE = 115200
     OPENOCD_PATH = '/opt/openocd-0.10.0/bin/openocd'
     DIRTY_SERIAL = True
-
-    AUTOTEST_AVAILABLE = [
-        'echo', 'get_time',  # mandatory
-        'leds_on', 'leds_off'
-    ]
-
-    ALIM = '5V'
-
-    def __init__(self):
-        self.serial_redirection = SerialRedirection(self.TTY, self.BAUDRATE)
-        self.openocd = OpenOCD.from_node(self)
-
-    def clear_serial(self):
-        """Clear serial link by flushing the input buffer."""
-        try:
-            ser = serial.Serial(self.TTY, self.BAUDRATE)
-        except serial.serialutil.SerialException:
-            LOGGER.error("No serial port found")
-            return 1
-        ser.reset_input_buffer()
-        ser.close()
-        return 0
-
-    @logger_call("Node ST_LINK: Setup of st-link node")
-    def setup(self, firmware_path):
-        """ Flash open node, create serial redirection """
-        ret_val = 0
-
-        common.wait_no_tty(self.TTY)
-        ret_val += common.wait_tty(self.TTY, LOGGER)
-        ret_val += self.flash(firmware_path)
-        ret_val += self.serial_redirection.start()
-        return ret_val
-
-    @logger_call("Node ST_LINK: teardown of st-link node")
-    def teardown(self):
-        """ Stop serial redirection and flash idle firmware """
-        ret_val = 0
-        # ON may have been stopped at the end of the experiment.
-        # And then restarted again in cn teardown.
-        # This leads to problem where the TTY disappears and reappears during
-        # the first 2 seconds. So let some time if it wants to disappear first.
-        common.wait_no_tty(self.TTY)
-        ret_val += common.wait_tty(self.TTY, LOGGER)
-        # cleanup debugger before flashing
-        ret_val += self.debug_stop()
-        ret_val += self.serial_redirection.stop()
-        ret_val += self.flash(None)
-        return ret_val
-
-    @logger_call("Node ST_LINK: flash of st-link node")
-    def flash(self, firmware_path=None):
-        """ Flash the given firmware on ST_LINK node
-
-        :param firmware_path: Path to the firmware to be flashed on `node`.
-                              If None, flash 'idle' firmware.
-        """
-        firmware_path = firmware_path or self.FW_IDLE
-        LOGGER.info('Flash firmware on %s: %s',
-                    self.TYPE.upper(), firmware_path)
-        ret = self.openocd.flash(firmware_path)
-        ret += self.clear_serial()
-        return ret
-
-    @logger_call("Node ST_LINK: reset of st-link node")
-    def reset(self):
-        """ Reset the ST_LINK node using jtag """
-        LOGGER.info('Reset %s node', self.TYPE.upper())
-        return self.openocd.reset()
-
-    def debug_start(self):
-        """ Start ST_LINK node debugger """
-        LOGGER.info('%s Node debugger start', self.TYPE.upper())
-        return self.openocd.debug_start()
-
-    def debug_stop(self):
-        """ Stop ST_LINK node debugger """
-        LOGGER.info('%s Node debugger stop', self.TYPE.upper())
-        return self.openocd.debug_stop()
-
-    @staticmethod
-    def status():
-        """ Check ST_LINK node status """
-        # Status is called when open node is not powered
-        # So can't check for FTDI
-        return 0
