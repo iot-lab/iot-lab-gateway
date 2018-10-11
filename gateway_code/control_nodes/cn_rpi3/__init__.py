cf0df47eec2821581c8c4a577dc61c4eeda55b22
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-10-11 10:48:04 +0200

    control_nodes/cn_rpi3: control start/stop of rtl_tcp daemon

--- gateway_code/control_nodes/cn_rpi3/__init__.py
+++ gateway_code/control_nodes/cn_rpi3/__init__.py
@@ -21,14 +21,20 @@
 
 """Control Node experiment implementation RPI3 ControlNode."""
 
+import os.path
+import time
 import logging
 import shlex
 
 from gateway_code.common import logger_call
 from gateway_code.nodes import ControlNodeBase
 from gateway_code.utils import subprocess_timeout
+from gateway_code.utils.rtl_tcp import RtlTcp
 
 LOGGER = logging.getLogger('gateway_code')
+
+LOCAL_CONFIG_DIR = '/var/local/config'
+RTL_TCP_CONFIG = os.path.join(LOCAL_CONFIG_DIR, 'rtl_sdr')
 
 # This command controls all 4 USB ports of the RPI3. The expected paramter is
 # either 0 (poweroff) or 1 (poweron)
@@ -50,12 +56,15 @@ class ControlNodeRpi3(ControlNodeBase):
     """ No Control Node """
     TYPE = 'rpi3'
     FEATURES = ['open_node_power']
+    RTL_TCP_PORT = 50000
+    RTL_TCP_FREQ = 868000000
 
     def __init__(self, node_id, default_profile):
         self.node_id = node_id
         self.default_profile = default_profile
         self.profile = self.default_profile
         self.open_node_state = 'stop'
+        self.rtl_tcp = RtlTcp(self.RTL_TCP_PORT, self.RTL_TCP_FREQ)
 
     @logger_call("Control node: Start")
     def start(self, exp_id, exp_files=None):  # pylint:disable=unused-argument
@@ -85,12 +94,19 @@ class ControlNodeRpi3(ControlNodeBase):
         ret = _call_cmd(UHUBCTL_CMD.format(1))
         if ret == 0:
             self.open_node_state = 'start'
+        if os.path.isfile(RTL_TCP_CONFIG):
+            ret += self.rtl_tcp.start()
+            LOGGER.debug("Process started: rtl tcp, ret: %d", ret)
         return ret
 
     @logger_call("Control node: stop power of open node")
     def open_stop(self, power=None):  # pylint:disable=unused-argument
         """ Stop open node with 'power' source """
-        ret = _call_cmd(UHUBCTL_CMD.format(0))
+        ret = 0
+        if os.path.isfile(RTL_TCP_CONFIG) and self.rtl_tcp.is_alive():
+            ret += self.rtl_tcp.stop()
+            LOGGER.debug("Process stopped: rtl tcp, ret: %d", ret)
+        ret += _call_cmd(UHUBCTL_CMD.format(0))
         if ret == 0:
             self.open_node_state = 'stop'
         return ret
@@ -112,7 +128,6 @@ class ControlNodeRpi3(ControlNodeBase):
         """Cleanup the control node configuration."""
         ret_val = 0
         ret_val += self.configure_profile(None)
-        ret_val += self.open_start('dc')
         return ret_val
 
     def autotest_setup(self, measures_handler):
