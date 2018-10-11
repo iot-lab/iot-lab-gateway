b7b864057515de1c8eb0b3f682acae5e3e8ec336
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-10-11 20:16:20 +0200

    utils/tests: add tests for new rtl_tcp/mjpg_streamer cli modules

---
+++ gateway_code/utils/tests/cli_mjpg_streamer_test.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+
+# This file is a part of IoT-LAB gateway_code
+# Copyright (C) 2015 INRIA (Contact: admin@iot-lab.info)
+# Contributor(s) : see AUTHORS file
+#
+# This software is governed by the CeCILL license under French law
+# and abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# http://www.cecill.info.
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+
+""" test mjpg_streamer module """
+
+import unittest
+
+import mock
+
+from ..cli import mjpg_streamer
+
+# pylint <= 1.3
+# pylint: disable=too-many-public-methods
+# pylint >= 1.4
+# pylint: disable=too-few-public-methods
+
+
+class TestMjpgStreamerMain(unittest.TestCase):
+    """ Main function tests """
+
+    @mock.patch('gateway_code.utils.mjpg_streamer.MjpgStreamer')
+    @mock.patch('signal.pause')
+    def test_main_function(self, m_pause, m_streamer_class):
+        """ Test cli.mjpg_streamer main function
+
+        Run and simulate 'stop' with a Ctrl+C
+        """
+        streamer = m_streamer_class.return_value
+        m_pause.side_effect = KeyboardInterrupt()
+
+        args = ['mjpg_streamer.py', '40000']
+        with mock.patch('sys.argv', args):
+            mjpg_streamer.main()
+
+            self.assertTrue(m_pause.called)
+            m_streamer_class.assert_called_with(40000)
+            self.assertTrue(streamer.start.called)
+            self.assertTrue(streamer.stop.called)
+
+
+class TestMjpgStreamerParsing(unittest.TestCase):
+    """ PARSER tests """
+
+    def test_parser(self):
+        """ Test cli.mjpg_streamer PARSER """
+        opts = mjpg_streamer.PARSER.parse_args(['40000'])
+        self.assertEquals(40000, opts.port)
