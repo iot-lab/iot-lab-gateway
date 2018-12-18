0157ea7487dafec98caa89e5d6017de2a477760b
Saint-Marcel Frederic <frederic.saint-marcel@inria.fr>
2018-12-18 17:00:24 +0100

    fix no-member lint error

--- gateway_code/nodes.py
+++ gateway_code/nodes.py
@@ -19,6 +19,8 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
+# pylint: disable=no-member
+
 """ Common logic for plugin nodes classes """
 import abc
 import os
