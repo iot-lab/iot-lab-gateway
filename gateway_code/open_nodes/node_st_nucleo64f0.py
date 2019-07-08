5c9ec199d17368a03a4753dc6be765a5f6e8068c
rienafairefr <rienafairefr@gmail.com>
2019-07-08 10:34:41 +0200

    initial support for st_nucleo64f0

---
+++ gateway_code/open_nodes/node_st_nucleo64f0.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+# This file is a part of IoT-LAB gateway_code
+# Copyright (C) 2015 INRIA (Contact: admin@iot-lab.info)
+# Contributor(s) : see AUTHORS file
+#
+# This software is governed by the CeCILL license under French law
+# and abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# http://www.cecill.info.
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+""" Open Node for Nucleo 64 F0 RB """
+from gateway_code.config import static_path
+from gateway_code.open_nodes.common.node_st_link import NodeStLinkBase
+
+
+class NodeStNucleo64f0(NodeStLinkBase):
+    # pylint:disable=no-member
+    """ Open node STM32 Nucleo 64 F0 St-Link based board implementation """
+
+    TYPE = 'st_nucleo64f0'
+    OPENOCD_CFG_FILE = static_path('iot-lab-st-nucleo64f0.cfg')
+    OPENOCD_PATH = '/opt/openocd-0.10.0/bin/openocd'
+    FW_IDLE = static_path('st-nucleo64f0_idle.elf')
+    FW_AUTOTEST = static_path('st-nucleo64f0_autotest.elf')
