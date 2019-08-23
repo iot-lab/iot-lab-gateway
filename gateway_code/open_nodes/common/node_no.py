64dc58c39f771278f06157dc3d3fdc51d3315503
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-08-23 10:34:03 +0200

    node_no: align flash method signature with other ONes

--- gateway_code/open_nodes/common/node_no.py
+++ gateway_code/open_nodes/common/node_no.py
@@ -44,7 +44,7 @@ class NodeNoBase(OpenNodeBase):
         return 0
 
     @logger_call("Node No: flash of no node")
-    def flash(self, firmware_path=None, binary=False, offset=None):
+    def flash(self, firmware_path=None, binary=False, offset=0):
         # pylint:disable=unused-argument,no-self-use
         """Does nothing."""
         return 0
