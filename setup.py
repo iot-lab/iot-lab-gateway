6fb02703ade7da4c70479eccd00aef5c51dbb500
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-01-18 15:15:44 +0100

    setup.py: update start/stop values for update_rc.d

--- setup.py
+++ setup.py
@@ -133,9 +133,11 @@ def post_install(self):
 
 def setup_initd_script(init_script):
     """Setup an init.d script."""
-    update_rc_d_args = ['update-rc.d', init_script,
-                        'start', '80', '2', '3', '4', '5', '.',
-                        'stop', '20', '0', '1', '6', '.']
+    update_rc_d_args = [
+        'update-rc.d', init_script,
+        'start', '85', '2', '3', '4', '5', '.',
+        'stop', '15', '0', '1', '6', '.'
+    ]
     shutil.copy('bin/init_script/' + init_script, '/etc/init.d/')
     os.chmod('/etc/init.d/' + init_script, 0o755)
     subprocess.check_call(update_rc_d_args)
